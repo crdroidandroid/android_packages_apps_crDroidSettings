b5d9d04c2232ef0bbb928f49d2c2683469039bcc
Roman Birg <roman@cyngn.com>
2015-11-11 14:59:28 -0500

    Add Quick Settings Pulldown options
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/StatusBar.java
+++ src/com/crdroid/settings/fragments/StatusBar.java
@@ -19,6 +19,7 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -34,15 +35,59 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.crdroid.settings.fragments.statusbar.NetworkTraffic;
 import com.crdroid.settings.R;
 
-public class StatusBar extends SettingsPreferenceFragment {
+public class StatusBar extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "StatusBar";
+
+    private static final String QUICK_PULLDOWN = "quick_pulldown";
+
+    private ListPreference mQuickPulldown;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.crdroid_settings_statusbar);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mQuickPulldown = (ListPreference) findPreference(QUICK_PULLDOWN);
+        int quickPulldownValue = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0);
+        mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
+        updatePulldownSummary(quickPulldownValue);
+        mQuickPulldown.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mQuickPulldown) {
+            int quickPulldownValue = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+                    quickPulldownValue, UserHandle.USER_CURRENT);
+            updatePulldownSummary(quickPulldownValue);
+            return true;
+        }
+        return false;
+    }
+
+    private void updatePulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // quick pulldown deactivated
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_off));
+        } else if (value == 3) {
+            // quick pulldown always
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary_always));
+        } else {
+            String direction = res.getString(value == 2
+                    ? R.string.quick_pulldown_left
+                    : R.string.quick_pulldown_right);
+            mQuickPulldown.setSummary(res.getString(R.string.quick_pulldown_summary, direction));
+        }
     }
 
     public static void reset(Context mContext) {
@@ -57,6 +102,8 @@ public class StatusBar extends SettingsPreferenceFragment {
                 Settings.System.BLUETOOTH_SHOW_BATTERY, 1);
         Settings.System.putInt(resolver,
                 Settings.System.SHOW_VOLTE_ICON, 0);
+        Settings.System.putInt(resolver,
+                Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0);
     }
 
     @Override
