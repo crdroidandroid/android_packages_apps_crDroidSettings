9259a9a849d41577b6a38a3e7ec808f86f7f6fed
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:48:30 +0200

    Add animations to quick settings tiles [2/2]
    Change-Id: Ia76d2f8d0cc6e3523973556fe162e645c86e1c7f
    [neo: Clean up patch.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/AnimationSettings.java
+++ src/com/crdroid/settings/fragments/AnimationSettings.java
@@ -2,6 +2,7 @@ package com.crdroid.settings.fragments;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -23,11 +24,15 @@ public class AnimationSettings extends SettingsPreferenceFragment
     private static final String KEY_LISTVIEW_ANIMATION = "listview_animation";
     private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
     private static final String POWER_MENU_ANIMATION = "power_menu_animation";
+    private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
+    private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
 
     private ListPreference mToastAnimation;
     private ListPreference mListViewAnimation;
     private ListPreference mListViewInterpolator;
     private ListPreference mPowerMenuAnimation;
+    private ListPreference mTileAnimationStyle;
+    private ListPreference mTileAnimationDuration;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -63,6 +68,23 @@ public class AnimationSettings extends SettingsPreferenceFragment
         mPowerMenuAnimation.setValue(String.valueOf(powermenuanimation));
         mPowerMenuAnimation.setSummary(mPowerMenuAnimation.getEntry());
         mPowerMenuAnimation.setOnPreferenceChangeListener(this);
+
+        mTileAnimationStyle = (ListPreference) findPreference(PREF_TILE_ANIM_STYLE);
+        int tileAnimationStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.ANIM_TILE_STYLE, 0,
+                UserHandle.USER_CURRENT);
+        mTileAnimationStyle.setValue(String.valueOf(tileAnimationStyle));
+        mTileAnimationStyle.setSummary(mTileAnimationStyle.getEntry());
+        mTileAnimationStyle.setOnPreferenceChangeListener(this);
+
+        mTileAnimationDuration = (ListPreference) findPreference(PREF_TILE_ANIM_DURATION);
+        int tileAnimationDuration = Settings.System.getIntForUser(resolver,
+                Settings.System.ANIM_TILE_DURATION, 2000,
+                UserHandle.USER_CURRENT);
+        mTileAnimationDuration.setValue(String.valueOf(tileAnimationDuration));
+        mTileAnimationDuration.setSummary(mTileAnimationDuration.getEntry());
+        mTileAnimationDuration.setEnabled(tileAnimationStyle > 0);
+        mTileAnimationDuration.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -96,6 +118,21 @@ public class AnimationSettings extends SettingsPreferenceFragment
             Settings.System.putInt(resolver,
                     Settings.System.POWER_MENU_ANIMATION, value);
             mPowerMenuAnimation.setSummary(mPowerMenuAnimation.getEntries()[index]);
+            return true;
+        } else if (preference == mTileAnimationStyle) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mTileAnimationStyle.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_STYLE,
+                    value, UserHandle.USER_CURRENT);
+            mTileAnimationStyle.setSummary(mTileAnimationStyle.getEntries()[index]);
+            mTileAnimationDuration.setEnabled(value > 0);
+            return true;
+       } else if (preference == mTileAnimationDuration) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mTileAnimationDuration.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_DURATION,
+                    value, UserHandle.USER_CURRENT);
+            mTileAnimationDuration.setSummary(mTileAnimationDuration.getEntries()[index]);
             return true;
         }
         return false;
