58985859edfa5700dba6c032129616f743d6d045
Nico60 <ndhouailly@gmail.com>
2016-05-29 13:05:37 +0200

    Add interpolators to qs tiles animation [2/2]
    Change-Id: I5144daf21f87bfdf3bdd855929f04849ca61ca33
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/AnimationSettings.java
+++ src/com/crdroid/settings/fragments/AnimationSettings.java
@@ -26,6 +26,7 @@ public class AnimationSettings extends SettingsPreferenceFragment
     private static final String POWER_MENU_ANIMATION = "power_menu_animation";
     private static final String PREF_TILE_ANIM_STYLE = "qs_tile_animation_style";
     private static final String PREF_TILE_ANIM_DURATION = "qs_tile_animation_duration";
+    private static final String PREF_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
 
     private ListPreference mToastAnimation;
     private ListPreference mListViewAnimation;
@@ -33,6 +34,7 @@ public class AnimationSettings extends SettingsPreferenceFragment
     private ListPreference mPowerMenuAnimation;
     private ListPreference mTileAnimationStyle;
     private ListPreference mTileAnimationDuration;
+    private ListPreference mTileAnimationInterpolator;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -85,6 +87,15 @@ public class AnimationSettings extends SettingsPreferenceFragment
         mTileAnimationDuration.setSummary(mTileAnimationDuration.getEntry());
         mTileAnimationDuration.setEnabled(tileAnimationStyle > 0);
         mTileAnimationDuration.setOnPreferenceChangeListener(this);
+
+        mTileAnimationInterpolator = (ListPreference) findPreference(PREF_TILE_ANIM_INTERPOLATOR);
+        int tileAnimationInterpolator = Settings.System.getIntForUser(resolver,
+                Settings.System.ANIM_TILE_INTERPOLATOR, 0,
+                UserHandle.USER_CURRENT);
+        mTileAnimationInterpolator.setValue(String.valueOf(tileAnimationInterpolator));
+        mTileAnimationInterpolator.setSummary(mTileAnimationInterpolator.getEntry());
+        mTileAnimationInterpolator.setEnabled(tileAnimationStyle > 0);
+        mTileAnimationInterpolator.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -126,6 +137,7 @@ public class AnimationSettings extends SettingsPreferenceFragment
                     value, UserHandle.USER_CURRENT);
             mTileAnimationStyle.setSummary(mTileAnimationStyle.getEntries()[index]);
             mTileAnimationDuration.setEnabled(value > 0);
+            mTileAnimationInterpolator.setEnabled(value > 0);
             return true;
        } else if (preference == mTileAnimationDuration) {
             int value = Integer.valueOf((String) newValue);
@@ -133,6 +145,13 @@ public class AnimationSettings extends SettingsPreferenceFragment
             Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_DURATION,
                     value, UserHandle.USER_CURRENT);
             mTileAnimationDuration.setSummary(mTileAnimationDuration.getEntries()[index]);
+            return true;
+       } else if (preference == mTileAnimationInterpolator) {
+            int value = Integer.valueOf((String) newValue);
+            int index = mTileAnimationInterpolator.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_INTERPOLATOR,
+                    value, UserHandle.USER_CURRENT);
+            mTileAnimationInterpolator.setSummary(mTileAnimationInterpolator.getEntries()[index]);
             return true;
         }
         return false;
