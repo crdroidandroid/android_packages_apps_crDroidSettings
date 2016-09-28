ba6c4137596f097425c8af86fa6d3c19ac5a1223
Lars Greiss <kufikugel@googlemail.com>
2016-09-28 21:15:04 +0200

    settings: less notifications sound (2/2)
    Change-Id: I61e085abc7754b2cd7119eddf2d8f3e6823fc099
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/SoundSettings.java
+++ src/com/crdroid/settings/fragments/SoundSettings.java
@@ -1,19 +1,45 @@
 package com.crdroid.settings.fragments;
 
-import com.android.internal.logging.MetricsProto.MetricsEvent;
-
+import android.content.ContentResolver;
 import android.os.Bundle;
-import com.android.settings.R;
+import android.provider.Settings.System;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class SoundSettings extends SettingsPreferenceFragment {
+public class SoundSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
+
+    private ListPreference mAnnoyingNotifications;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.crdroid_settings_sound);
+
+        mAnnoyingNotifications = (ListPreference) findPreference(PREF_LESS_NOTIFICATION_SOUNDS);
+        int notificationThreshold = System.getInt(getContentResolver(),
+                System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, 0);
+        mAnnoyingNotifications.setValue(Integer.toString(notificationThreshold));
+        mAnnoyingNotifications.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mAnnoyingNotifications) {
+            int val = Integer.valueOf((String) newValue);
+            System.putInt(getContentResolver(),
+                    System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, val);
+            return true;
+        }
+        return false;
     }
 
     @Override
