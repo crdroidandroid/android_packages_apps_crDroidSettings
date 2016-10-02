4bdb00a9dd0873da1a35597c9db8faa8b853c3dc
Pranav Vashi <neobuddy89@gmail.com>
2016-10-02 20:26:19 +0530

    Settings: Disable suggestions [3/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/MiscSettings.java
+++ src/com/crdroid/settings/fragments/MiscSettings.java
@@ -13,11 +13,11 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.Preference;
+import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
@@ -27,11 +27,13 @@ import java.util.HashSet;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class MiscSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+public class MiscSettings extends SettingsPreferenceFragment
+    implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
+    private static final String DISABLE_SETTINGS_SUGGESTIONS = "disable_settings_suggestions";
+    private SwitchPreference mDisableSuggestions;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,12 +45,25 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         if (!DevelopmentSettings.isPackageInstalled(getActivity(), KEY_LOCK_CLOCK_PACKAGE_NAME)) {
             getPreferenceScreen().removePreference(findPreference(KEY_LOCK_CLOCK));
         }
+
+        mDisableSuggestions =
+                (SwitchPreference) findPreference(DISABLE_SETTINGS_SUGGESTIONS);
+
+        if (mDisableSuggestions != null) {
+            mDisableSuggestions.setChecked((Settings.System.getInt(getContentResolver(),
+                  Settings.System.DISABLE_SETTINGS_SUGGESTIONS, 0) == 1));
+            mDisableSuggestions.setOnPreferenceChangeListener(this);
+        }
     }
 
-    @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
-    return false;
+        if (preference == mDisableSuggestions) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.DISABLE_SETTINGS_SUGGESTIONS, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
