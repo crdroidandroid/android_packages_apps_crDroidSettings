d982c356596b3bfeec5fc5692c98a58d08947ced
Pranav Vashi <neobuddy89@gmail.com>
2016-10-04 23:37:33 +0530

    crdroid: Add option to disable Navbar[2/2]
    Change-Id: I62e520186630bc0709b736694879130b2b3adb2c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/NavigationBarSettings.java
+++ src/com/crdroid/settings/fragments/NavigationBarSettings.java
@@ -1,47 +1,65 @@
 package com.crdroid.settings.fragments;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.os.PowerManager;
-import android.preference.CheckBoxPreference;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceManager;
-import android.preference.PreferenceScreen;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.preference.SwitchPreference;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
-import com.android.settings.R;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
+import com.android.settings.R;
+import com.android.settings.SeekBarPreference;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
-import com.android.settings.SettingsPreferenceFragment;
+public class NavigationBarSettings extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
 
+    private static final String TAG = "NavBar";
+    private static final String ENABLE_NAVIGATION_BAR = "enable_nav_bar";
 
-public class NavigationBarSettings extends SettingsPreferenceFragment {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        addPreferencesFromResource(R.xml.crdroid_settings_navigation);
-    }
+    private SwitchPreference mEnableNavigationBar;
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.CRDROID_SETTINGS;
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.crdroid_settings_navigation);
+
+        final Resources res = getActivity().getResources();
+
+        // navigation bar switch
+        mEnableNavigationBar = (SwitchPreference) findPreference(ENABLE_NAVIGATION_BAR);
+        mEnableNavigationBar.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.NAVIGATION_BAR_SHOW, 1) == 1);
+        mEnableNavigationBar.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mEnableNavigationBar) {
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_SHOW,
+                    ((Boolean) newValue) ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 }
+
