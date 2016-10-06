e2ae333a7312fc5809dbddd3de289c58281ef796
Pranav Vashi <neobuddy89@gmail.com>
2016-10-06 20:44:18 +0530

    crdroid: Add Sound category
    Change-Id: I2483c9169a1031482c8849edbcb634c66be71602
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ src/com/crdroid/settings/fragments/SoundSettings.java
@@ -0,0 +1,23 @@
+package com.crdroid.settings.fragments;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class SoundSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.crdroid_settings_sound);
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.CRDROID_SETTINGS;
+    }
+}
