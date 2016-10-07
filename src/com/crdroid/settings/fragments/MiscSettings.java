5ce4e8a378747a49bb8fbf151ad11981d31bc8c1
Pranav Vashi <neobuddy89@gmail.com>
2016-10-07 21:55:59 +0530

    crdroid: Clean up Misc Settings
    Change-Id: Iff7ec706862889aa8cff68bcc5338735407d6b56
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/MiscSettings.java
+++ src/com/crdroid/settings/fragments/MiscSettings.java
@@ -13,11 +13,11 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.support.v7.preference.ListPreference;
-import android.support.v14.preference.SwitchPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceCategory;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
@@ -27,15 +27,11 @@ import java.util.HashSet;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class MiscSettings extends SettingsPreferenceFragment
-    implements Preference.OnPreferenceChangeListener {
+public class MiscSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
 
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
-    private static final String DISABLE_SETTINGS_SUGGESTIONS = "disable_settings_suggestions";
-    private static final String REMOVE_SETTINGS_SUMMARY = "remove_settings_summary";
-    private SwitchPreference mDisableSuggestions;
-    private SwitchPreference mRemoveSummary;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,40 +43,12 @@ public class MiscSettings extends SettingsPreferenceFragment
         if (!DevelopmentSettings.isPackageInstalled(getActivity(), KEY_LOCK_CLOCK_PACKAGE_NAME)) {
             getPreferenceScreen().removePreference(findPreference(KEY_LOCK_CLOCK));
         }
-
-        mDisableSuggestions =
-                (SwitchPreference) findPreference(DISABLE_SETTINGS_SUGGESTIONS);
-
-        if (mDisableSuggestions != null) {
-            mDisableSuggestions.setChecked((Settings.System.getInt(getContentResolver(),
-                  Settings.System.DISABLE_SETTINGS_SUGGESTIONS, 0) == 1));
-            mDisableSuggestions.setOnPreferenceChangeListener(this);
-        }
-
-        mRemoveSummary =
-                (SwitchPreference) findPreference(REMOVE_SETTINGS_SUMMARY);
-
-        if (mRemoveSummary != null) {
-            mRemoveSummary.setChecked((Settings.System.getInt(getContentResolver(),
-                  Settings.System.REMOVE_SETTINGS_SUMMARY, 0) == 1));
-            mRemoveSummary.setOnPreferenceChangeListener(this);
-        }
-
     }
 
+    @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mDisableSuggestions) {
-            boolean value = (Boolean) objValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.DISABLE_SETTINGS_SUGGESTIONS, value ? 1 : 0);
-            return true;
-        } else if (preference == mRemoveSummary) {
-            boolean value = (Boolean) objValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.REMOVE_SETTINGS_SUMMARY, value ? 1 : 0);
-            return true;
-        }
-        return false;
+
+    return false;
     }
 
     @Override
