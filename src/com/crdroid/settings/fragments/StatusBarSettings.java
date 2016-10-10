aedf2162a129737b6ae9ac4738b742833359e5dc
Pranav Vashi <neobuddy89@gmail.com>
2016-10-10 20:19:17 +0530

    Rework crDroid logo [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/StatusBarSettings.java
+++ src/com/crdroid/settings/fragments/StatusBarSettings.java
@@ -9,11 +9,12 @@ import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
 import android.content.ContentResolver;
 import android.content.res.Resources;
-import android.preference.ListPreference;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.PreferenceScreen;
-import android.preference.SwitchPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 import com.android.settings.R;
 
@@ -28,20 +29,65 @@ import android.util.Log;
 import java.util.List;
 import java.util.ArrayList;
 
-public class StatusBarSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+import com.crdroid.settings.preferences.ColorPickerPreference;
+
+public class StatusBarSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_CRDROID_LOGO_COLOR = "status_bar_crdroid_logo_color";
+    private static final String KEY_CRDROID_LOGO_STYLE = "status_bar_crdroid_logo_style";
+
+    private ColorPickerPreference mCrDroidLogoColor;
+    private ListPreference mCrDroidLogoStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.crdroid_settings_statusbar);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
 
+        mCrDroidLogoStyle = (ListPreference) findPreference(KEY_CRDROID_LOGO_STYLE);
+        int crdroidLogoStyle = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUS_BAR_CRDROID_LOGO_STYLE, 0,
+                UserHandle.USER_CURRENT);
+        mCrDroidLogoStyle.setValue(String.valueOf(crdroidLogoStyle));
+        mCrDroidLogoStyle.setSummary(mCrDroidLogoStyle.getEntry());
+        mCrDroidLogoStyle.setOnPreferenceChangeListener(this);
+
+        // CrDroid logo color
+        mCrDroidLogoColor =
+                (ColorPickerPreference) prefSet.findPreference(KEY_CRDROID_LOGO_COLOR);
+        mCrDroidLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_CRDROID_LOGO_COLOR, 0xffffffff);
+        String hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mCrDroidLogoColor.setSummary(hexColor);
+        mCrDroidLogoColor.setNewPreviewColor(intColor);
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mCrDroidLogoColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(resolver,
+                Settings.System.STATUS_BAR_CRDROID_LOGO_COLOR, intHex);
+            return true;
+        } else if (preference == mCrDroidLogoStyle) {
+            int crdroidLogoStyle = Integer.valueOf((String) newValue);
+            int index = mCrDroidLogoStyle.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(
+                resolver, Settings.System.STATUS_BAR_CRDROID_LOGO_STYLE, crdroidLogoStyle,
+                UserHandle.USER_CURRENT);
+            mCrDroidLogoStyle.setSummary(
+                    mCrDroidLogoStyle.getEntries()[index]);
+            return true;
+        }
         return false;
     }
 
