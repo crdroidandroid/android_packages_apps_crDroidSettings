d00cc4b19de369e40d380d3257c6dbdf41cc3c92
Pranav Vashi <neobuddy89@gmail.com>
2016-10-21 23:35:16 +0530

    crDroid: Bring-in working CM status bar options
    Change-Id: I87599b9ad691a73161298affc194d7c1451adfbf
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/StatusBarSettings.java
+++ src/com/crdroid/settings/fragments/StatusBarSettings.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.os.UserHandle;
+import android.content.res.Configuration;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.support.v7.preference.ListPreference;
@@ -15,37 +15,38 @@ import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
+import android.text.format.DateFormat;
 import android.provider.Settings;
-import com.android.settings.R;
-
-import java.util.Locale;
-import android.text.TextUtils;
+import android.os.UserHandle;
 import android.view.View;
 
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
-import android.util.Log;
-
-import java.util.List;
-import java.util.ArrayList;
 
 import com.crdroid.settings.preferences.ColorPickerPreference;
+
+import cyanogenmod.preference.CMSystemSettingListPreference;
 
 public class StatusBarSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_CRDROID_LOGO_COLOR = "status_bar_crdroid_logo_color";
     private static final String KEY_CRDROID_LOGO_STYLE = "status_bar_crdroid_logo_style";
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
+    private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
 
     private ColorPickerPreference mCrDroidLogoColor;
     private ListPreference mCrDroidLogoStyle;
+    private CMSystemSettingListPreference mStatusBarClock;
+    private CMSystemSettingListPreference mStatusBarAmPm;
+    private CMSystemSettingListPreference mQuickPulldown;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.crdroid_settings_statusbar);
-        PreferenceScreen prefSet = getPreferenceScreen();
         final ContentResolver resolver = getActivity().getContentResolver();
 
         mCrDroidLogoStyle = (ListPreference) findPreference(KEY_CRDROID_LOGO_STYLE);
@@ -58,13 +59,36 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 
         // CrDroid logo color
         mCrDroidLogoColor =
-                (ColorPickerPreference) prefSet.findPreference(KEY_CRDROID_LOGO_COLOR);
+                (ColorPickerPreference) findPreference(KEY_CRDROID_LOGO_COLOR);
         mCrDroidLogoColor.setOnPreferenceChangeListener(this);
         int intColor = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_CRDROID_LOGO_COLOR, 0xffffffff);
         String hexColor = String.format("#%08x", (0xffffffff & intColor));
         mCrDroidLogoColor.setSummary(hexColor);
         mCrDroidLogoColor.setNewPreviewColor(intColor);
+
+        mStatusBarClock = (CMSystemSettingListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
+        mStatusBarAmPm = (CMSystemSettingListPreference) findPreference(STATUS_BAR_AM_PM);
+        mQuickPulldown = (CMSystemSettingListPreference) findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
+
+        if (DateFormat.is24HourFormat(getActivity())) {
+            mStatusBarAmPm.setEnabled(false);
+            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
+        }
+
+        updatePulldownSummary(mQuickPulldown.getIntValue(0));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Adjust clock position for RTL if necessary
+        Configuration config = getResources().getConfiguration();
+        if (config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
+                mStatusBarClock.setEntries(getActivity().getResources().getStringArray(
+                        R.array.status_bar_clock_style_entries_rtl));
+                mStatusBarClock.setSummary(mStatusBarClock.getEntry());
+        }
     }
 
     @Override
@@ -94,6 +118,20 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.CRDROID_SETTINGS;
+    }
+
+    private void updatePulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // quick pulldown deactivated
+            mQuickPulldown.setSummary(res.getString(R.string.status_bar_quick_qs_pulldown_off));
+        } else {
+            String direction = res.getString(value == 2
+                    ? R.string.status_bar_quick_qs_pulldown_summary_left
+                    : R.string.status_bar_quick_qs_pulldown_summary_right);
+            mQuickPulldown.setSummary(res.getString(R.string.status_bar_quick_qs_pulldown_summary, direction));
+        }
     }
 
 }
