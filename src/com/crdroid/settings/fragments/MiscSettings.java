dc98afe803529724841611e10570550d98012c74
Pranav Vashi <neobuddy89@gmail.com>
2016-10-30 14:52:33 +0530

    crdroid: add CPU info overlay [2/2]
    Change-Id: I95a4891bb2395a0e861b6d5afa9ae990290b2681
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/MiscSettings.java
+++ src/com/crdroid/settings/fragments/MiscSettings.java
@@ -1,37 +1,27 @@
 package com.crdroid.settings.fragments;
 
-import com.android.internal.logging.MetricsProto.MetricsEvent;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
-import android.content.ContentResolver;
-import android.content.DialogInterface;
-import android.content.res.Resources;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemProperties;
-import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.Preference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.SwitchPreference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
-
-import java.util.Arrays;
-import java.util.HashSet;
-
 import com.android.settings.SettingsPreferenceFragment;
 
-public class MiscSettings extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+public class MiscSettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
+    private static final String SHOW_CPU_INFO_KEY = "show_cpu_info";
+    private SwitchPreference mShowCpuInfo;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,12 +33,32 @@ public class MiscSettings extends SettingsPreferenceFragment implements
         if (!DevelopmentSettings.isPackageInstalled(getActivity(), KEY_LOCK_CLOCK_PACKAGE_NAME)) {
             getPreferenceScreen().removePreference(findPreference(KEY_LOCK_CLOCK));
         }
+
+        mShowCpuInfo = (SwitchPreference) findPreference(SHOW_CPU_INFO_KEY);
+        mShowCpuInfo.setChecked(Settings.Global.getInt(getActivity().getContentResolver(),
+                Settings.Global.SHOW_CPU, 0) == 1);
+        mShowCpuInfo.setOnPreferenceChangeListener(this);
+        }
+
+    private void writeCpuInfoOptions(boolean value) {
+        Settings.Global.putInt(getActivity().getContentResolver(),
+                Settings.Global.SHOW_CPU, value ? 1 : 0);
+        Intent service = (new Intent())
+                .setClassName("com.android.systemui", "com.android.systemui.CPUInfoService");
+        if (value) {
+            getActivity().startService(service);
+        } else {
+            getActivity().stopService(service);
+        }
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-
-    return false;
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mShowCpuInfo) {
+            writeCpuInfoOptions((Boolean) newValue);
+            return true;
+        }
+        return false;
     }
 
     @Override
