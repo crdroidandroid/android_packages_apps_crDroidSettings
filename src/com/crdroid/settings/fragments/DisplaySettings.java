e97b0a52776c8886c5bd1800ae435992fd7f0e31
Pranav Vashi <neobuddy89@gmail.com>
2016-12-10 01:06:20 +0530

    crdroid: Fix up doze switch
    Change-Id: Idcedc230e1803c4fd65778504be0d8be1906d2f3
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/DisplaySettings.java
+++ src/com/crdroid/settings/fragments/DisplaySettings.java
@@ -1,19 +1,13 @@
 package com.crdroid.settings.fragments;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemProperties;
-import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
-import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -31,6 +25,7 @@ public class DisplaySettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.crdroid_settings_display);
 
         mDozeFragement = (SwitchPreference) findPreference(KEY_DOZE_FRAGMENT);
+        mDozeFragement.setChecked(isDozeEnabled());
         mDozeFragement.setOnPreferenceChangeListener(this);
     }
 
@@ -47,14 +42,16 @@ public class DisplaySettings extends SettingsPreferenceFragment
     @Override
     public void onResume() {
         super.onResume();
-        boolean dozeEnabled = Settings.Secure.getInt(
-                getContentResolver(), Settings.Secure.DOZE_ENABLED,
-                getActivity().getResources().getBoolean(
-                com.android.internal.R.bool.config_doze_enabled_by_default) ? 1 : 0) != 0;
         if (mDozeFragement != null) {
-            mDozeFragement.setSummary(dozeEnabled
+            mDozeFragement.setSummary(isDozeEnabled()
                     ? R.string.summary_doze_enabled : R.string.summary_doze_disabled);
         }
+    }
+
+    public boolean isDozeEnabled() {
+        return Settings.Secure.getInt(getContentResolver(), Settings.Secure.DOZE_ENABLED,
+                getActivity().getResources().getBoolean(
+                com.android.internal.R.bool.config_doze_enabled_by_default) ? 1 : 0) != 0;
     }
 
     @Override
