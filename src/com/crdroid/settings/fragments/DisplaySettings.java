6de4d50c16868cf603bf3bdaa77206b0a2beef20
beanstown106 <nbenis106@gmail.com>
2017-01-10 10:01:49 +0800

    Custom ambient display settings (2/2)
    Change-Id: I9837809216d499da059ad42f94bb90e311aeb26e
    [neo: This is revival of old code.]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/DisplaySettings.java
+++ src/com/crdroid/settings/fragments/DisplaySettings.java
@@ -1,13 +1,19 @@
 package com.crdroid.settings.fragments;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.DevelopmentSettings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -16,7 +22,7 @@ public class DisplaySettings extends SettingsPreferenceFragment
 
     private static final String KEY_DOZE_FRAGMENT = "doze_fragment";
 
-    private SwitchPreference mDozeFragement;
+    private PreferenceScreen mDozeFragement;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,37 +30,23 @@ public class DisplaySettings extends SettingsPreferenceFragment
 
         addPreferencesFromResource(R.xml.crdroid_settings_display);
 
-        mDozeFragement = (SwitchPreference) findPreference(KEY_DOZE_FRAGMENT);
-        mDozeFragement.setChecked(isDozeEnabled());
-        mDozeFragement.setOnPreferenceChangeListener(this);
+        mDozeFragement = (PreferenceScreen) findPreference(KEY_DOZE_FRAGMENT);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mDozeFragement) {
-            boolean value = (Boolean) newValue;
-            Settings.Secure.putInt(getContentResolver(), Settings.Secure.DOZE_ENABLED, value ? 1 : 0);
-            updateStatus();
-            return true;
-        }
         return false;
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        updateStatus();
-    }
-
-    public boolean isDozeEnabled() {
-        return Settings.Secure.getInt(getContentResolver(), Settings.Secure.DOZE_ENABLED,
+        boolean dozeEnabled = Settings.Secure.getInt(
+                getContentResolver(), Settings.Secure.DOZE_ENABLED,
                 getActivity().getResources().getBoolean(
                 com.android.internal.R.bool.config_doze_enabled_by_default) ? 1 : 0) != 0;
-    }
-
-    public void updateStatus() {
         if (mDozeFragement != null) {
-            mDozeFragement.setSummary(isDozeEnabled()
+            mDozeFragement.setSummary(dozeEnabled
                     ? R.string.summary_doze_enabled : R.string.summary_doze_disabled);
         }
     }
