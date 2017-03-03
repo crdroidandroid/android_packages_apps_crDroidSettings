0ce8848e4be3ca73439484ff6107ac4d61746ff9
Pranav Vashi <neobuddy89@gmail.com>
2017-03-04 01:10:27 +0530

    [2/3] crdroid: single hand for hardware keys
    Change-Id: I821f986c95127d83dcc10bbd957242c7a3bbfac6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/ButtonSettings.java
+++ src/com/crdroid/settings/fragments/ButtonSettings.java
@@ -86,7 +86,9 @@ public class ButtonSettings extends SettingsPreferenceFragment
         LAUNCH_CAMERA,
         SLEEP,
         LAST_APP,
-        SPLIT_SCREEN;
+        SPLIT_SCREEN,
+        SINGLE_HAND_LEFT,
+        SINGLE_HAND_RIGHT;
 
         public static Action fromIntSafe(int id) {
             if (id < NOTHING.ordinal() || id > Action.values().length) {
