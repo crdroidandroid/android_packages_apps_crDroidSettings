9b32d2a8cf45f6759e070a759fed0931f90b290e
Pranav Vashi <neobuddy89@gmail.com>
2018-04-02 00:47:28 +0530

    Move system default animation settings under Global [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/ui/AnimationControls.java
+++ src/com/crdroid/settings/fragments/ui/AnimationControls.java
@@ -280,8 +280,8 @@ public class AnimationControls extends SettingsPreferenceFragment implements OnP
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
-        Settings.System.putIntForUser(resolver,
-                Settings.System.SYSTEM_DEFAULT_ANIMATION, 0, UserHandle.USER_CURRENT);
+        Settings.Global.putInt(resolver,
+                Settings.Global.SYSTEM_DEFAULT_ANIMATION, 0);
         Settings.System.putIntForUser(resolver,
                 Settings.System.ANIMATION_CONTROLS_DURATION, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
