a83e1f47694f900eaa367cfc0d7bdb3a89e30d67
Alexandr <razziell@yandex.by>
2018-05-09 12:38:20 +0300

    Choose a custom editor app for screenshot notification edit button [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -16,10 +16,13 @@
 package com.crdroid.settings.fragments;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -32,13 +35,22 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.development.DevelopmentSettings;
 import com.android.settings.SettingsPreferenceFragment;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
 import com.crdroid.settings.fragments.misc.AlarmBlocker;
 import com.crdroid.settings.fragments.misc.ScreenStateToggles;
 import com.crdroid.settings.fragments.misc.WakeLockBlocker;
+import com.crdroid.settings.fragments.misc.ScreenshotEditPackageListAdapter;
+import com.crdroid.settings.fragments.misc.ScreenshotEditPackageListAdapter.PackageItem;
 import com.crdroid.settings.R;
 
 public class Miscellaneous extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
 
     public static final String TAG = "Miscellaneous";
 
@@ -46,9 +58,12 @@ public class Miscellaneous extends SettingsPreferenceFragment
     private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
     private static final String SHOW_CPU_INFO_KEY = "show_cpu_info";
     private static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
+    private static final int DIALOG_SCREENSHOT_EDIT_APP = 1;
 
     private SwitchPreference mShowCpuInfo;
     private ListPreference mMSOB;
+    private Preference mScreenshotEditAppPref;
+    private ScreenshotEditPackageListAdapter mPackageAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,6 +72,11 @@ public class Miscellaneous extends SettingsPreferenceFragment
         Context mContext = getActivity().getApplicationContext();
 
         addPreferencesFromResource(R.xml.crdroid_settings_misc);
+
+        //Screenshot edit app
+        mPackageAdapter = new ScreenshotEditPackageListAdapter(getActivity());
+        mScreenshotEditAppPref = findPreference("screenshot_edit_app");
+        mScreenshotEditAppPref.setOnPreferenceClickListener(this);
 
         // mLockClock
         if (!DevelopmentSettings.isPackageInstalled(mContext, KEY_LOCK_CLOCK_PACKAGE_NAME)) {
@@ -75,6 +95,33 @@ public class Miscellaneous extends SettingsPreferenceFragment
         mMSOB.setValue(String.valueOf(mMSOBValue));
         mMSOB.setSummary(mMSOB.getEntry());
         mMSOB.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public Dialog onCreateDialog(int dialogId) {
+        switch (dialogId) {
+            case DIALOG_SCREENSHOT_EDIT_APP: {
+                Dialog dialog;
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+                final ListView list = new ListView(getActivity());
+                list.setAdapter(mPackageAdapter);
+                alertDialog.setTitle(R.string.profile_choose_app);
+                alertDialog.setView(list);
+                dialog = alertDialog.create();
+                list.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        // Add empty application definition, the user will be able to edit it later
+                        PackageItem info = (PackageItem) parent.getItemAtPosition(position);
+                        Settings.System.putString(getActivity().getContentResolver(),
+                                Settings.System.SCREENSHOT_EDIT_USER_APP, info.packageName);
+                        dialog.cancel();
+                    }
+                });
+                return dialog;
+            }
+         }
+        return super.onCreateDialog(dialogId);
     }
 
     private static void writeCpuInfoOptions(Context mContext, boolean value) {
@@ -128,7 +175,30 @@ public class Miscellaneous extends SettingsPreferenceFragment
     }
 
     @Override
+    public int getDialogMetricsCategory(int dialogId) {
+        switch (dialogId) {
+            case DIALOG_SCREENSHOT_EDIT_APP:
+                return MetricsEvent.CRDROID_SETTINGS;
+            default:
+                return 0;
+        }
+     }
+
+    @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
     }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        // Don't show the dialog if there are no available editor apps
+        if (preference == mScreenshotEditAppPref && mPackageAdapter.getCount() > 0) {
+            showDialog(DIALOG_SCREENSHOT_EDIT_APP);
+        } else {
+            Toast.makeText(getActivity(), getActivity().getString(R.string.screenshot_edit_app_no_editor),
+                    Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }
+
 }
