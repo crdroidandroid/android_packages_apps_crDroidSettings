d991c1bf9b9550d6be1178f05d9c04197d04fd9c
Pranav Vashi <neobuddy89@gmail.com>
2018-09-24 08:53:17 +0530

    crdroid: Re-enable available animations
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/ui/Animations.java
+++ src/com/crdroid/settings/fragments/ui/Animations.java
@@ -13,75 +13,54 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.crdroid.settings.fragments;
+package com.crdroid.settings.fragments.ui;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.os.UserHandle;
-import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.search.Indexable;
 
 import com.crdroid.settings.R;
-import com.crdroid.settings.fragments.ui.Animations;
-import com.crdroid.settings.fragments.ui.DozeFragment;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import lineageos.providers.LineageSettings;
-
-public class UserInterface extends SettingsPreferenceFragment implements Indexable {
+public class Animations extends SettingsPreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.crdroid_settings_ui);
+        addPreferencesFromResource(R.xml.animations);
     }
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
-        Animations.reset(mContext);
-        DozeFragment.reset(mContext);
+        Settings.Global.putInt(resolver,
+                Settings.Global.SCREEN_OFF_ANIMATION, 0);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.ANIM_TILE_STYLE, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.ANIM_TILE_DURATION, 2000, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.ANIM_TILE_INTERPOLATOR, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
     }
-
-    /**
-     * For search
-     */
-    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider() {
-                @Override
-                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                        boolean enabled) {
-                    ArrayList<SearchIndexableResource> result =
-                            new ArrayList<SearchIndexableResource>();
-                    SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.crdroid_settings_ui;
-                    result.add(sir);
-
-                    return result;
-                }
-            };
 }
