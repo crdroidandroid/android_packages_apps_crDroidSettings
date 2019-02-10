ee747e7e134acf0d180026a11984daf08dec9664
Pranav Vashi <neobuddy89@gmail.com>
2019-02-10 18:31:55 +0530

    Remove signature spoofing toggle
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -86,8 +86,6 @@ public class Miscellaneous extends SettingsPreferenceFragment
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
         Settings.Global.putInt(resolver,
-                Settings.Global.ALLOW_SIGNATURE_FAKE, 1);
-        Settings.Global.putInt(resolver,
                 Settings.Global.TOAST_ICON, 1);
         Settings.System.putIntForUser(resolver,
                 Settings.System.MEDIA_SCANNER_ON_BOOT, 0, UserHandle.USER_CURRENT);
