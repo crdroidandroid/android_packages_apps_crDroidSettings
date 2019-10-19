34c1296e7aee0cb9cb7a92f720ded7ee818c7a44
Pranav Vashi <neobuddy89@gmail.com>
2019-10-20 02:14:55 +0530

    PebbleBerry: Revive
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/ui/ThemeSettings.java
+++ src/com/crdroid/settings/fragments/ui/ThemeSettings.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2016-2019 crDroid Android Project
+ * Copyright (C) 2018-2019 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,19 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.crdroid.settings.fragments;
 
-import android.content.BroadcastReceiver;
+package com.crdroid.settings.fragments.ui;
+
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
-import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.os.ServiceManager;
-import android.os.SystemProperties;
+import android.os.Handler;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
@@ -36,35 +34,57 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 
-import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.util.crdroid.Utils;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 
 import com.crdroid.settings.R;
-import com.crdroid.settings.fragments.ui.Animations;
-import com.crdroid.settings.fragments.ui.RoundedCorners;
-import com.crdroid.settings.fragments.ui.ThemeSettings;
+import com.crdroid.settings.fragments.ui.AccentPicker;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
-import lineageos.providers.LineageSettings;
+public class ThemeSettings extends SettingsPreferenceFragment implements Indexable,
+        Preference.OnPreferenceChangeListener {
 
-public class UserInterface extends SettingsPreferenceFragment implements Indexable {
+    public static final String TAG = "ThemeSettings";
+
+    private String KEY_ACCENT_PICKER = "berry_accent_picker";
+
+    private Preference mAccentPicker;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.crdroid_settings_ui);
+        addPreferencesFromResource(R.xml.theme_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mAccentPicker = findPreference(KEY_ACCENT_PICKER);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mAccentPicker) {
+            AccentPicker.show(this);
+        }
+
+        return super.onPreferenceTreeClick(preference);
     }
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
-        Animations.reset(mContext);
-        RoundedCorners.reset(mContext);
-        ThemeSettings.reset(mContext);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.BERRY_ACCENT_PICKER, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.BERRY_THEME_OVERRIDE, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
@@ -83,17 +103,10 @@ public class UserInterface extends SettingsPreferenceFragment implements Indexab
                     ArrayList<SearchIndexableResource> result =
                             new ArrayList<SearchIndexableResource>();
                     SearchIndexableResource sir = new SearchIndexableResource(context);
-                    sir.xmlResId = R.xml.crdroid_settings_ui;
+                    sir.xmlResId = R.xml.theme_settings;
                     result.add(sir);
 
                     return result;
-                }
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-
-                    return keys;
                 }
             };
 }
