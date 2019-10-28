f9f9084e2018862ad9eee3d6237c9d87be678d6e
Pranav Vashi <neobuddy89@gmail.com>
2019-10-28 16:02:13 +0530

    Add Lockscreen album art filter [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/LockScreen.java
+++ src/com/crdroid/settings/fragments/LockScreen.java
@@ -68,6 +68,8 @@ public class LockScreen extends SettingsPreferenceFragment
         ContentResolver resolver = mContext.getContentResolver();
         LineageSettings.Secure.putIntForUser(resolver,
                 LineageSettings.Secure.LOCKSCREEN_MEDIA_METADATA, 1, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.LOCKSCREEN_ALBUMART_FILTER, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT);
         LockScreenVisualizer.reset(mContext);
