489628455fff641a3430a544b83a8bebfe52d95b
Pranav Vashi <neobuddy89@gmail.com>
2019-11-08 00:27:54 +0530

    crdroid: Re-enable smart pixel settings
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/UserInterface.java
+++ src/com/crdroid/settings/fragments/UserInterface.java
@@ -45,6 +45,7 @@ import com.android.settings.search.Indexable;
 import com.crdroid.settings.R;
 import com.crdroid.settings.fragments.ui.Animations;
 import com.crdroid.settings.fragments.ui.RoundedCorners;
+import com.crdroid.settings.fragments.ui.SmartPixels;
 import com.crdroid.settings.fragments.ui.ThemeSettings;
 
 import java.util.ArrayList;
@@ -54,16 +55,30 @@ import lineageos.providers.LineageSettings;
 
 public class UserInterface extends SettingsPreferenceFragment implements Indexable {
 
+    private static final String SMART_PIXELS = "smart_pixels";
+
+    private Preference mSmartPixels;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.crdroid_settings_ui);
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources res = getResources();
+
+        mSmartPixels = (Preference) prefScreen.findPreference(SMART_PIXELS);
+        boolean mSmartPixelsSupported = res.getBoolean(
+                com.android.internal.R.bool.config_supportSmartPixels);
+        if (!mSmartPixelsSupported)
+            prefScreen.removePreference(mSmartPixels);
     }
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
         Animations.reset(mContext);
         RoundedCorners.reset(mContext);
+        SmartPixels.reset(mContext);
         ThemeSettings.reset(mContext);
     }
 
