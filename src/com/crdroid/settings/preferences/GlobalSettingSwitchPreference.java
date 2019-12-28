3f4d061c2d3a921c1a6718225c9770b24f768a7b
Pranav Vashi <neobuddy89@gmail.com>
2019-12-28 19:56:24 +0530

    crdroid: Update global settings switch
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/preferences/GlobalSettingSwitchPreference.java
+++ src/com/crdroid/settings/preferences/GlobalSettingSwitchPreference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2017 crDroid Android
+ * Copyright (C) 2016-2019 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +17,13 @@ package com.crdroid.settings.preferences;
 
 import android.content.Context;
 import android.provider.Settings;
-import androidx.preference.SwitchPreference;
+import android.os.UserHandle;
 import android.util.AttributeSet;
 
-public class GlobalSettingSwitchPreference extends SwitchPreference {
+import lineageos.preference.SelfRemovingSwitchPreference;
+
+public class GlobalSettingSwitchPreference extends SelfRemovingSwitchPreference {
+
     public GlobalSettingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
@@ -30,34 +33,22 @@ public class GlobalSettingSwitchPreference extends SwitchPreference {
     }
 
     public GlobalSettingSwitchPreference(Context context) {
-        super(context, null);
+        super(context);
     }
 
     @Override
-    protected boolean persistBoolean(boolean value) {
-        if (shouldPersist()) {
-            if (value == getPersistedBoolean(!value)) {
-                // It's already there, so the same as persisting
-                return true;
-            }
-            Settings.Global.putInt(getContext().getContentResolver(), getKey(), value ? 1 : 0);
-            return true;
-        }
-        return false;
+    protected boolean isPersisted() {
+        return Settings.Global.getString(getContext().getContentResolver(), getKey()) != null;
     }
 
     @Override
-    protected boolean getPersistedBoolean(boolean defaultReturnValue) {
-        if (!shouldPersist()) {
-            return defaultReturnValue;
-        }
+    protected void putBoolean(String key, boolean value) {
+        Settings.Global.putInt(getContext().getContentResolver(), key, value ? 1 : 0);
+    }
+
+    @Override
+    protected boolean getBoolean(String key, boolean defaultValue) {
         return Settings.Global.getInt(getContext().getContentResolver(),
-                getKey(), defaultReturnValue ? 1 : 0) != 0;
-    }
-
-    @Override
-    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-        setChecked(Settings.Global.getString(getContext().getContentResolver(), getKey()) != null ? getPersistedBoolean(isChecked())
-                : (Boolean) defaultValue);
+                key, defaultValue ? 1 : 0) != 0;
     }
 }
