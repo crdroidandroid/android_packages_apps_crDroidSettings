def2a7b49f89fb56248c0485cc6b9ca0ed7f71d6
Pranav Vashi <neobuddy89@gmail.com>
2019-12-28 20:03:34 +0530

    crdroid: Allow to use old style icons as default [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/StatusBar.java
+++ src/com/crdroid/settings/fragments/StatusBar.java
@@ -65,12 +65,14 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String KEY_SHOW_FOURG = "show_fourg_icon";
     private static final String KEY_SHOW_ROAMING = "roaming_indicator_icon";
     private static final String KEY_SHOW_VOLTE = "show_volte_icon";
+    private static final String KEY_OLD_MOBILETYPE = "use_old_mobiletype";
 
     private LineageSystemSettingListPreference mStatusBarClock;
     private SwitchPreference mDataDisabled;
     private SwitchPreference mShowFourg;
     private SwitchPreference mShowRoaming;
     private SwitchPreference mShowVolte;
+    private SwitchPreference mOldMobileType;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -79,6 +81,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.crdroid_settings_statusbar);
 
         ContentResolver resolver = getActivity().getContentResolver();
+        Context mContext = getActivity().getApplicationContext();
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
 
@@ -113,6 +116,15 @@ public class StatusBar extends SettingsPreferenceFragment implements
             prefScreen.removePreference(mShowRoaming);
             prefScreen.removePreference(mShowVolte);
         }
+
+        mOldMobileType = (SwitchPreference) findPreference(KEY_OLD_MOBILETYPE);
+        boolean mConfigUseOldMobileType = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_useOldMobileIcons);
+
+        boolean showing = Settings.System.getIntForUser(resolver,
+                Settings.System.USE_OLD_MOBILETYPE,
+                mConfigUseOldMobileType ? 1 : 0, UserHandle.USER_CURRENT) != 0;
+        mOldMobileType.setChecked(showing);
     }
 
     @Override
@@ -122,6 +134,8 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
+        boolean mConfigUseOldMobileType = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_useOldMobileIcons);
 
         LineageSettings.System.putIntForUser(resolver,
                 LineageSettings.System.DOUBLE_TAP_SLEEP_GESTURE, 1, UserHandle.USER_CURRENT);
@@ -148,7 +162,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
-                Settings.System.USE_OLD_MOBILETYPE, 0, UserHandle.USER_CURRENT);
+                Settings.System.USE_OLD_MOBILETYPE, mConfigUseOldMobileType ? 1 : 0, UserHandle.USER_CURRENT);
 
         BatteryBar.reset(mContext);
         Clock.reset(mContext);
