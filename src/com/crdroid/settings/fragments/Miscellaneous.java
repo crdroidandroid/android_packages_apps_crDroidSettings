195f6140a73c4415ea6cbf02b08f91d6d539f3de
Pranav Vashi <neobuddy89@gmail.com>
2019-12-29 15:59:56 +0530

    crdroid: Implement Aggressive Battery [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -46,6 +46,7 @@ import com.android.settings.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
 
 import com.crdroid.settings.R;
+import com.crdroid.settings.fragments.misc.AggressiveBattery;
 import com.crdroid.settings.fragments.misc.GamingMode;
 import com.crdroid.settings.fragments.misc.ImeSettings;
 
@@ -89,6 +90,7 @@ public class Miscellaneous extends SettingsPreferenceFragment
         Settings.System.putIntForUser(resolver,
                 Settings.System.THREE_FINGER_GESTURE, 0, UserHandle.USER_CURRENT);
         writeCpuInfoOptions(mContext, false);
+        AggressiveBattery.reset(mContext);
         GamingMode.reset(mContext);
         ImeSettings.reset(mContext);
     }
