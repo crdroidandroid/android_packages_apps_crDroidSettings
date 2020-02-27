d93de622a1b0cc5de557fd4c0a99c38ea2be4785
Pranav Vashi <neobuddy89@gmail.com>
2020-02-28 00:20:59 +0530

    Alert Slider: Add toggle to disable notifications [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/Notifications.java
+++ src/com/crdroid/settings/fragments/Notifications.java
@@ -46,9 +46,11 @@ public class Notifications extends SettingsPreferenceFragment {
 
     public static final String TAG = "Notifications";
 
+    private static final String ALERT_SLIDER_PREF = "alert_slider_notifications";
     private static final String BATTERY_LIGHTS_PREF = "battery_lights";
     private static final String NOTIFICATION_LIGHTS_PREF = "notification_lights";
 
+    private Preference mAlertSlider;
     private Preference mBatLights;
     private Preference mNotLights;
     private PreferenceCategory lightsCategory;
@@ -63,6 +65,12 @@ public class Notifications extends SettingsPreferenceFragment {
 
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
+
+        mAlertSlider = (Preference) prefScreen.findPreference(ALERT_SLIDER_PREF);
+        boolean mAlertSliderAvailable = res.getBoolean(
+                com.android.internal.R.bool.config_hasAlertSlider);
+        if (!mAlertSliderAvailable)
+            prefScreen.removePreference(mAlertSlider);
 
         mBatLights = (Preference) prefScreen.findPreference(BATTERY_LIGHTS_PREF);
         boolean mBatLightsSupported = res.getInteger(
@@ -86,6 +94,8 @@ public class Notifications extends SettingsPreferenceFragment {
         ContentResolver resolver = mContext.getContentResolver();
         Settings.Global.putInt(resolver,
                 Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, 1);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.ALERT_SLIDER_NOTIFICATIONS, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.LESS_BORING_HEADS_UP, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
@@ -113,6 +123,11 @@ public class Notifications extends SettingsPreferenceFragment {
                 public List<String> getNonIndexableKeys(Context context) {
                     List<String> keys = super.getNonIndexableKeys(context);
                     final Resources res = context.getResources();
+
+                    boolean mAlertSliderAvailable = res.getBoolean(
+                            com.android.internal.R.bool.config_hasAlertSlider);
+                    if (!mAlertSliderAvailable)
+                        keys.add(ALERT_SLIDER_PREF);
 
                     boolean mBatLightsSupported = res.getInteger(
                             org.lineageos.platform.internal.R.integer.config_deviceLightCapabilities) >= 64;
