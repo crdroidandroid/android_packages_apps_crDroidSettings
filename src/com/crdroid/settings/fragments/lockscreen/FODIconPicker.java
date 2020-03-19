5393e78bdcb1925dafe08754b00ceb60e7f6e42a
raystef66 <s.vanbarel@gmail.com>
2020-03-19 11:17:34 +0000

    crDroid Settings: add FOD invisible icon [2/2]

--- src/com/crdroid/settings/fragments/lockscreen/FODIconPicker.java
+++ src/com/crdroid/settings/fragments/lockscreen/FODIconPicker.java
@@ -66,6 +66,7 @@ public class FODIconPicker extends LayoutPreference {
     private static ImageButton Button19;
     private static ImageButton Button20;
     private static ImageButton Button21;
+    private static ImageButton Button22;
 
     private static final String TAG = "FODIconPicker";
 
@@ -127,6 +128,7 @@ public class FODIconPicker extends LayoutPreference {
         Button19 = findViewById(R.id.fodicon19_button);
         Button20 = findViewById(R.id.fodicon20_button);
         Button21 = findViewById(R.id.fodicon21_button);
+        Button22 = findViewById(R.id.fodicon22_button);
 
         int defaultfodicon = Settings.System.getInt(
                 context.getContentResolver(), Settings.System.FOD_ICON, 0);
@@ -174,6 +176,8 @@ public class FODIconPicker extends LayoutPreference {
             updateHighlightedItem(Button20, context);
         } else if (defaultfodicon == 21) {
             updateHighlightedItem(Button21, context);
+        } else if (defaultfodicon == 22) {
+            updateHighlightedItem(Button22, context);
         }
 
         Button0.setOnClickListener(new View.OnClickListener() {
@@ -330,6 +334,13 @@ public class FODIconPicker extends LayoutPreference {
                 updateHighlightedItem(Button21, context);
             }
         });
+        Button22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateSettings(22, context);
+                updateHighlightedItem(Button22, context);
+            }
+        });
     }
 
     private void updateSettings(int fodicon, Context context) {
@@ -361,6 +372,7 @@ public class FODIconPicker extends LayoutPreference {
         Button19.setBackgroundTintList(defaulttint);
         Button20.setBackgroundTintList(defaulttint);
         Button21.setBackgroundTintList(defaulttint);
+        Button22.setBackgroundTintList(defaulttint);
         activebutton.setBackgroundTintList(Utils.getColorAttr(getContext(), android.R.attr.colorAccent));
     }
 }
