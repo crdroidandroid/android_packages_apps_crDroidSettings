3e66510f3ba927e899bd4292ac7ee6ebf6053e00
Lup Gabriel <gwolf2u.website@gmail.com>
2020-04-11 21:58:36 +0300

    Revert "QS Header data usage info [2/2]"
    This reverts commit 01165dc65ebd66c86e01f3ae6032b2f3963e5375.

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -89,8 +89,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements Indexab
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_BATTERY_STYLE, -1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
-                Settings.System.QS_DATAUSAGE, 0, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
                 Settings.System.QS_SHOW_BATTERY_PERCENT, 2, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_SHOW_BATTERY_ESTIMATE, 0, UserHandle.USER_CURRENT);
