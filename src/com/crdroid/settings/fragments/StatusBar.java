e1acec65a30d9211998a7027f4bc7da9d2e84861
Pranav Vashi <neobuddy89@gmail.com>
2020-10-07 20:24:51 +0530

    crdroid: Add more VoWiFi icon styles [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/StatusBar.java
+++ src/com/crdroid/settings/fragments/StatusBar.java
@@ -58,9 +58,11 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
     private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
     private static final String KEY_VOLTE_ICON_STYLE = "volte_icon_style";
+    private static final String KEY_VOWIFI_ICON_STYLE = "vowifi_icon_style";
 
     private LineageSystemSettingListPreference mStatusBarClock;
     private SystemSettingSeekBarPreference mVolteIconStyle;
+    private SystemSettingSeekBarPreference mVowifiIconStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -91,9 +93,11 @@ public class StatusBar extends SettingsPreferenceFragment implements
         }
 
         mVolteIconStyle = (SystemSettingSeekBarPreference) findPreference(KEY_VOLTE_ICON_STYLE);
+        mVowifiIconStyle = (SystemSettingSeekBarPreference) findPreference(KEY_VOWIFI_ICON_STYLE);
 
         if (!TelephonyUtils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(mVolteIconStyle);
+            prefScreen.removePreference(mVowifiIconStyle);
         }
     }
 
@@ -116,6 +120,8 @@ public class StatusBar extends SettingsPreferenceFragment implements
                 Settings.System.BLUETOOTH_SHOW_BATTERY, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.VOLTE_ICON_STYLE, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.VOWIFI_ICON_STYLE, 0, UserHandle.USER_CURRENT);
 
         Clock.reset(mContext);
         NetworkTrafficSettings.reset(mContext);
@@ -138,6 +144,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
 
                     if (!TelephonyUtils.isVoiceCapable(context)) {
                         keys.add(KEY_VOLTE_ICON_STYLE);
+                        keys.add(KEY_VOWIFI_ICON_STYLE);
                     }
 
                     return keys;
