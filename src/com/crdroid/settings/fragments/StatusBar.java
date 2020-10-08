14076490c0e5dd62829e79195c8ae20dc5bb0746
Pranav Vashi <neobuddy89@gmail.com>
2020-10-08 21:51:44 +0530

    crdroid: status bar battery customizations
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/StatusBar.java
+++ src/com/crdroid/settings/fragments/StatusBar.java
@@ -146,6 +146,12 @@ public class StatusBar extends SettingsPreferenceFragment implements
                 Settings.System.SHOW_FOURG_ICON, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.DATA_DISABLED_ICON, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.STATUS_BAR_BATTERY_STYLE, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.STATUS_BAR_BATTERY_TEXT_CHARGING, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
 
         Clock.reset(mContext);
         NetworkTrafficSettings.reset(mContext);
