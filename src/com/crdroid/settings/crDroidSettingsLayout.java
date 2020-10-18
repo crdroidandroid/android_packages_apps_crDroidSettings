614095e3e1dcbc155c6a56bdbeb1e81d19b35e5a
Pranav Vashi <neobuddy89@gmail.com>
2020-10-19 03:54:06 +0530

    crdroid: Bring up lockscreen fragment
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/crDroidSettingsLayout.java
+++ src/com/crdroid/settings/crDroidSettingsLayout.java
@@ -42,12 +42,12 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 import com.crdroid.settings.fragments.About;
+import com.crdroid.settings.fragments.LockScreen;
 import com.crdroid.settings.fragments.Navigation;
 import com.crdroid.settings.fragments.QuickSettings;
 import com.crdroid.settings.fragments.StatusBar;
 /*
 import com.crdroid.settings.fragments.Buttons;
-import com.crdroid.settings.fragments.LockScreen;
 import com.crdroid.settings.fragments.Miscellaneous;
 import com.crdroid.settings.fragments.Notifications;
 import com.crdroid.settings.fragments.Sound;
@@ -137,12 +137,12 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
 /*
             Recents.reset(rContext);
             Buttons.reset(rContext);
-            LockScreen.reset(rContext);
             Miscellaneous.reset(rContext);
             Notifications.reset(rContext);
             Sound.reset(rContext);
             UserInterface.reset(rContext);
 */
+            LockScreen.reset(rContext);
             Navigation.reset(rContext);
             QuickSettings.reset(rContext);
             StatusBar.reset(rContext);
@@ -172,7 +172,6 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
             super(fm);
 /*
             frags[3] = new Recents();
-            frags[2] = new LockScreen();
             frags[4] = new Buttons();
             frags[5] = new UserInterface();
             frags[6] = new Notifications();
@@ -181,8 +180,9 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
 */
             frags[0] = new StatusBar();
             frags[1] = new QuickSettings();
-            frags[2] = new Navigation();
-            frags[3] = new About();
+            frags[2] = new LockScreen();
+            frags[3] = new Navigation();
+            frags[4] = new About();
         }
 
         @Override
@@ -206,7 +206,6 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
         titleString = new String[] {
 /*
             getString(R.string.recents_title),
-            getString(R.string.lockscreen_title),
             getString(R.string.button_title),
             getString(R.string.ui_title),
             getString(R.string.notifications_title),
@@ -215,6 +214,7 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
 */
             getString(R.string.statusbar_title),
             getString(R.string.quicksettings_title),
+            getString(R.string.lockscreen_title),
             getString(R.string.navbar_title),
             getString(R.string.about_crdroid)
         };
---
+++ src/com/crdroid/settings/fragments/LockScreen.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2016-2020 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.fragments;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import lineageos.providers.LineageSettings;
+
+@SearchIndexable
+public class LockScreen extends SettingsPreferenceFragment
+            implements Preference.OnPreferenceChangeListener  {
+
+    public static final String TAG = "LockScreen";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.crdroid_settings_lockscreen);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        LineageSettings.Secure.putIntForUser(resolver,
+                LineageSettings.Secure.LOCKSCREEN_MEDIA_METADATA, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.DOUBLE_TAP_SLEEP_LOCKSCREEN, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_ALBUMART_FILTER, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_BATTERY_INFO, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_CHARGING_ANIMATION_STYLE, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_LOCK_ICON, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_STATUS_BAR, 1, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+    }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.crdroid_settings_lockscreen);
+}
