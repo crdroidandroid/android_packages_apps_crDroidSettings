b7c6386fca394fd682622eeed9a0257c8640d99d
Pranav Vashi <neobuddy89@gmail.com>
2020-11-26 09:25:57 +0530

    crdroid: Bring up UI fragment
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/crDroidSettingsLayout.java
+++ src/com/crdroid/settings/crDroidSettingsLayout.java
@@ -50,8 +50,8 @@ import com.crdroid.settings.fragments.Notifications;
 import com.crdroid.settings.fragments.QuickSettings;
 import com.crdroid.settings.fragments.Sound;
 import com.crdroid.settings.fragments.StatusBar;
-/*
 import com.crdroid.settings.fragments.UserInterface;
+/*
 import com.crdroid.settings.fragments.Recents;
 */
 
@@ -136,7 +136,6 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
         protected Void doInBackground(Void... params) {
 /*
             Recents.reset(rContext);
-            UserInterface.reset(rContext);
 */
             Buttons.reset(rContext);
             LockScreen.reset(rContext);
@@ -146,6 +145,7 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
             QuickSettings.reset(rContext);
             Sound.reset(rContext);
             StatusBar.reset(rContext);
+            UserInterface.reset(rContext);
             finish();
             startActivity(getIntent());
             return null;
@@ -172,17 +172,17 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
             super(fm);
 /*
             frags[3] = new Recents();
-            frags[5] = new UserInterface();
 */
             frags[0] = new StatusBar();
             frags[1] = new QuickSettings();
             frags[2] = new LockScreen();
             frags[3] = new Navigation();
             frags[4] = new Buttons();
-            frags[5] = new Notifications();
-            frags[6] = new Sound();
-            frags[7] = new Miscellaneous();
-            frags[8] = new About();
+            frags[5] = new UserInterface();
+            frags[6] = new Notifications();
+            frags[7] = new Sound();
+            frags[8] = new Miscellaneous();
+            frags[9] = new About();
         }
 
         @Override
@@ -206,13 +206,13 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
         titleString = new String[] {
 /*
             getString(R.string.recents_title),
-            getString(R.string.ui_title),
 */
             getString(R.string.statusbar_title),
             getString(R.string.quicksettings_title),
             getString(R.string.lockscreen_title),
             getString(R.string.navbar_title),
             getString(R.string.button_title),
+            getString(R.string.ui_title),
             getString(R.string.notifications_title),
             getString(R.string.sound_title),
             getString(R.string.misc_title),
---
+++ src/com/crdroid/settings/fragments/UserInterface.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2016-2020 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.fragments;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.crdroid.Utils;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.crdroid.settings.fragments.ui.SmartPixels;
+import com.crdroid.settings.utils.DeviceUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SearchIndexable
+public class UserInterface extends SettingsPreferenceFragment {
+
+    private static final String SMART_PIXELS = "smart_pixels";
+
+    private Preference mSmartPixels;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.crdroid_settings_ui);
+
+        Context mContext = getActivity().getApplicationContext();
+
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources res = getResources();
+
+        mSmartPixels = (Preference) prefScreen.findPreference(SMART_PIXELS);
+        boolean mSmartPixelsSupported = res.getBoolean(
+                com.android.internal.R.bool.config_supportSmartPixels);
+        if (!mSmartPixelsSupported)
+            prefScreen.removePreference(mSmartPixels);
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+        SmartPixels.reset(mContext);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+    }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.crdroid_settings_ui) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    final Resources res = context.getResources();
+
+                    boolean mSmartPixelsSupported = res.getBoolean(
+                            com.android.internal.R.bool.config_supportSmartPixels);
+                    if (!mSmartPixelsSupported)
+                        keys.add(SMART_PIXELS);
+
+                    return keys;
+                }
+            };
+}
