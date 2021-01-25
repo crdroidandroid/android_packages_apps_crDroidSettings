7357e0b32b8b6eede9ac1f4eafb58255bf47526a
Lars Greiss <kufikugel@googlemail.com>
2021-01-25 17:15:43 +0300

    Added modded marginov color picker
    Squashed commit of the following:
    
    commit 404001f88a5e59e4bc19baff3a7b4042b897b3ca
    Author: ezio84 <brabus84@gmail.com>
    Date:   Mon Sep 18 12:50:12 2017 +0200
    
        Change color picker reset icon
    
        Change-Id: Ia63f3cc7d69d3f59a464dc6e26805f80898e4cbc
    
    commit 149e44b73c25fadb5f3fc175ce7c81906c915350
    Author: ezio84 <brabus84@gmail.com>
    Date:   Mon Sep 18 09:12:57 2017 +0200
    
        Color Picker: don't show picker on click if preference is disabled
    
    commit c9367477ff6dfeafa4b69ad37a77a6b877a3c1ad
    Author: ezio84 <brabus84@gmail.com>
    Date:   Sun Sep 17 15:04:30 2017 +0200
    
        Color picker: don't show keyboard on start
    
    commit 79f648044079e0fc79aa3f895633f66f3f332947
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Tue Jan 31 08:51:05 2017 +0100
    
        Replace colorpicker action set png with vector
    
        Change-Id: Ie44e1e91234706bb80f1ac56c72a68e1fe8eff07
    
    commit ebc044a258a0f664ee8fada4efa3ae8079365e3c
    Author: ezio84 <brabus84@gmail.com>
    Date:   Wed Jan 4 16:02:15 2017 +0100
    
        margaritov color picker: add a proper landscape layout
    
        preview:
        https://s30.postimg.org/dvr6fyza9/Screenshot_20170104_155921.png
    
    commit 511e335f8d0549b6b4cd341867faf7d347bcbcd3
    Author: Randall Rushing <randall.rushing@gmail.com>
    Date:   Mon Aug 31 04:21:55 2015 -0700
    
        ColorPickerPreference: Introduce set default button when attribute is set
    
        Set a default value using the "defaultColorValue" settings namespace
        attribute. The general idea is often we want set a value of say "-1"
        and let framework handle it. One such scenario would be if you are
        changing the color of a ImageView using ColorFilter. When framework
        receives a "-1" value, framework sets the ImageView ColorFilter to null,
        restoring the ImageView to original state.
    
        Screenshot:
    
        https://teameos.slack.com/files/bigrushdog/F09T5S155/screenshot_2015-08-31-04-02-54.png
    
        Change-Id: I4ad63576270de344a9e7430d9e63a248d37afd9f
    
    commit d04cdb95110cdf382219b4186703eca18d2e04d4
    Author: Lars Greiss <kufikugel@googlemail.com>
    Date:   Fri Dec 19 00:18:41 2014 -0500
    
        Added modded marginov color picker
    
        -- Adapt for Preference being part of support library
    
        Change-Id: Ic721fdc7f63607d972d203e1a13097e8870bce2f
    
    Change-Id: I32760fbc4bc51ae2c0ab04f79f5a8d17990be177

---
+++ src/net/margaritov/preference/colorpicker/ColorPickerDialog.java
@@ -0,0 +1,193 @@
+/*
+ * Copyright (C) 2010 Daniel Nilsson
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.margaritov.preference.colorpicker;
+
+import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.android.settings.R;
+
+public class ColorPickerDialog extends AlertDialog implements ColorPickerView.OnColorChangedListener, View.OnClickListener {
+
+    private ColorPickerView mColorPicker;
+    private ColorPickerPanelView mOldColor;
+    private ColorPickerPanelView mNewColor;
+    private EditText mHex;
+
+    private boolean mShowLedPreview;
+
+    private NotificationManager mNoMan;
+    private Context mContext;
+
+    private OnColorChangedListener mListener;
+
+    public interface OnColorChangedListener {
+        void onColorChanged(int color);
+    }
+
+    ColorPickerDialog(Context context, int initialColor, boolean showLedPreview) {
+        super(context);
+        mContext = context;
+        mShowLedPreview = showLedPreview;
+        init(initialColor);
+    }
+
+    private void init(int color) {
+        if (getWindow() != null) {
+            getWindow().setFormat(PixelFormat.RGBA_8888);
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setUp(color);
+        }
+    }
+
+    private void setUp(int color) {
+
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
+        mNoMan = (NotificationManager)
+                mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        assert inflater != null;
+        View layout = inflater.inflate(R.layout.cr_dialog_color_picker, null);
+
+        mColorPicker = layout.findViewById(R.id.color_picker_view);
+        mOldColor = layout.findViewById(R.id.old_color_panel);
+        mNewColor = layout.findViewById(R.id.new_color_panel);
+
+        mHex = layout.findViewById(R.id.hex);
+        ImageButton mSetButton = layout.findViewById(R.id.enter);
+
+        ((LinearLayout) mOldColor.getParent()).setPadding(Math.round(mColorPicker.getDrawingOffset()),
+                0, Math.round(mColorPicker.getDrawingOffset()), 0);
+
+        mOldColor.setOnClickListener(this);
+        mNewColor.setOnClickListener(this);
+        mColorPicker.setOnColorChangedListener(this);
+        mOldColor.setColor(color);
+        mColorPicker.setColor(color, true);
+        showLed(color);
+
+        if (mHex != null) {
+            mHex.setText(ColorPickerPreference.convertToARGB(color));
+        }
+        if (mSetButton != null) {
+            mSetButton.setOnClickListener(v -> {
+                String text = mHex.getText().toString();
+                try {
+                    int newColor = ColorPickerPreference.convertToColorInt(text);
+                    mColorPicker.setColor(newColor, true);
+                } catch (Exception ignored) {
+                }
+            });
+        }
+
+        setView(layout);
+    }
+
+    @Override
+    public void onColorChanged(int color) {
+
+        mNewColor.setColor(color);
+        try {
+            if (mHex != null) {
+                mHex.setText(ColorPickerPreference.convertToARGB(color));
+            }
+        } catch (Exception ignored) {
+        }
+        showLed(color);
+    }
+
+    private void showLed(int color) {
+        if (mShowLedPreview) {
+            if (color == 0xFFFFFFFF) {
+                // argb white doesn't work
+                color = 0xffffff;
+            }
+            mNoMan.forceShowLedLight(color);
+        }
+    }
+
+    private void switchOffLed() {
+        if (mShowLedPreview) {
+            mNoMan.forceShowLedLight(-1);
+        }
+    }
+
+    void setAlphaSliderVisible(boolean visible) {
+        mColorPicker.setAlphaSliderVisible(visible);
+    }
+
+    /**
+     * Set a OnColorChangedListener to get notified when the color selected by the user has changed.
+     *
+     * @param listener
+     */
+    void setOnColorChangedListener(OnColorChangedListener listener) {
+        mListener = listener;
+    }
+
+    public int getColor() {
+        return mColorPicker.getColor();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.new_color_panel) {
+            if (mListener != null) {
+                mListener.onColorChanged(mNewColor.getColor());
+            }
+        }
+        dismiss();
+        switchOffLed();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        switchOffLed();
+    }
+
+
+    @NonNull
+    @Override
+    public Bundle onSaveInstanceState() {
+        Bundle state = super.onSaveInstanceState();
+        state.putInt("old_color", mOldColor.getColor());
+        state.putInt("new_color", mNewColor.getColor());
+        dismiss();
+        switchOffLed();
+        return state;
+    }
+
+    @Override
+    public void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mOldColor.setColor(savedInstanceState.getInt("old_color"));
+        mColorPicker.setColor(savedInstanceState.getInt("new_color"), true);
+    }
+}
