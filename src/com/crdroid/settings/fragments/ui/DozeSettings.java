3dc74f9b5f99cedb1030aa3bf71a3c2090625103
Pranav Vashi <neobuddy89@gmail.com>
2021-02-09 17:43:12 +0530

    Ambient Music Ticker - Allow to pulse on new tracks [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/ui/DozeSettings.java
+++ src/com/crdroid/settings/fragments/ui/DozeSettings.java
@@ -218,6 +218,8 @@ public class DozeSettings extends SettingsPreferenceFragment implements
                 Settings.Secure.PULSE_AMBIENT_LIGHT_DURATION, 2, UserHandle.USER_CURRENT);
         Settings.Secure.putIntForUser(resolver,
                 Settings.Secure.PULSE_AMBIENT_LIGHT_LAYOUT, 0, UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.PULSE_ON_NEW_TRACKS, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
