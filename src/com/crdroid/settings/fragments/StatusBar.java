2e5a1da99590c49fedf560d04ef1e59e04bdad41
Adam Szorenyi <adam.szorenyi88@gmail.com>
2021-03-22 21:36:01 +0100

    crdroid: Smart Pulldown [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/StatusBar.java
+++ src/com/crdroid/settings/fragments/StatusBar.java
@@ -35,6 +35,7 @@ import androidx.preference.SwitchPreference;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.crdroid.settings.preferences.SystemSettingListPreference;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
@@ -53,6 +54,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     public static final String TAG = "StatusBar";
 
     private static final String QUICK_PULLDOWN = "qs_quick_pulldown";
+    private static final String SMART_PULLDOWN = "qs_smart_pulldown";
     private static final String KEY_VOLTE_ICON_STYLE = "volte_icon_style";
     private static final String KEY_SHOW_ROAMING = "roaming_indicator_icon";
     private static final String KEY_SHOW_FOURG = "show_fourg_icon";
@@ -64,6 +66,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final int PULLDOWN_DIR_ALWAYS = 3;
 
     private LineageSystemSettingListPreference mQuickPulldown;
+    private SystemSettingListPreference mSmartPulldown;
     private SystemSettingSeekBarPreference mVolteIconStyle;
     private SwitchPreference mShowRoaming;
     private SwitchPreference mShowFourg;
@@ -94,6 +97,10 @@ public class StatusBar extends SettingsPreferenceFragment implements
         mQuickPulldown.setOnPreferenceChangeListener(this);
         updateQuickPulldownSummary(mQuickPulldown.getIntValue(0));
 
+        mSmartPulldown = (SystemSettingListPreference) findPreference(SMART_PULLDOWN);
+        mSmartPulldown.setOnPreferenceChangeListener(this);
+        updateSmartPulldownSummary(mSmartPulldown.getIntValue(0));
+
         // Adjust status bar preferences for RTL
         if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
             mQuickPulldown.setEntries(R.array.status_bar_quick_qs_pulldown_entries_rtl);
@@ -106,6 +113,10 @@ public class StatusBar extends SettingsPreferenceFragment implements
         if (preference == mQuickPulldown) {
             int value = Integer.parseInt((String) newValue);
             updateQuickPulldownSummary(value);
+            return true;
+        } else if (preference == mSmartPulldown) {
+            int value = Integer.parseInt((String) newValue);
+            updateSmartPulldownSummary(value);
             return true;
         }
         return false;
@@ -127,6 +138,8 @@ public class StatusBar extends SettingsPreferenceFragment implements
                 Settings.System.DATA_DISABLED_ICON, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.BLUETOOTH_SHOW_BATTERY, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_SMART_PULLDOWN, 0, UserHandle.USER_CURRENT);
     }
 
     private void updateQuickPulldownSummary(int value) {
@@ -150,6 +163,22 @@ public class StatusBar extends SettingsPreferenceFragment implements
                 break;
         }
         mQuickPulldown.setSummary(summary);
+    }
+
+    private void updateSmartPulldownSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            // Smart pulldown deactivated
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_off));
+        } else if (value == 3) {
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_none_summary));
+        } else {
+            String type = res.getString(value == 1
+                    ? R.string.smart_pulldown_dismissable
+                    : R.string.smart_pulldown_ongoing);
+            mSmartPulldown.setSummary(res.getString(R.string.smart_pulldown_summary, type));
+        }
     }
 
     @Override
