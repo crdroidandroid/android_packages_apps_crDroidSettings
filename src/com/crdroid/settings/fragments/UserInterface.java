5856f141f471a82d723cdd5b09433e7d00a029c5
AgentFabulous <kshitijgm@gmail.com>
2021-06-04 20:35:14 +0200

    [SQUASHED] crDroidSettings: Introduce RGB Gradient Picker+RGB Accent picker
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>

--- src/com/crdroid/settings/fragments/UserInterface.java
+++ src/com/crdroid/settings/fragments/UserInterface.java
@@ -22,12 +22,15 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.content.om.IOverlayManager;
+import android.content.om.OverlayInfo;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -45,17 +48,29 @@ import com.android.settingslib.search.SearchIndexable;
 import com.crdroid.settings.fragments.ui.CutoutSettings;
 import com.crdroid.settings.fragments.ui.DozeSettings;
 import com.crdroid.settings.fragments.ui.SmartPixels;
+import com.crdroid.settings.preferences.colorpicker.ColorPickerPreference;
+
 import com.crdroid.settings.utils.DeviceUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SearchIndexable
-public class UserInterface extends SettingsPreferenceFragment {
+public class UserInterface extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener{
 
+    private static final int DEFAULT_COLOR = 0xff1a73e8;
+
+    private static final String ACCENT_COLOR = "accent_color";
+    private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
+    private static final String GRADIENT_COLOR = "gradient_color";
+    private static final String GRADIENT_COLOR_PROP = "persist.sys.theme.gradientcolor";
     private static final String SMART_PIXELS = "smart_pixels";
     private static final String DISPLAY_CUTOUT = "cutout_settings";
 
+    private IOverlayManager mOverlayService;
+    private ColorPickerPreference mThemeColor;
+    private ColorPickerPreference mGradientColor;
     private Preference mSmartPixels;
     private Preference mDisplayCutout;
 
@@ -66,6 +81,10 @@ public class UserInterface extends SettingsPreferenceFragment {
 
         Context mContext = getActivity().getApplicationContext();
 
+        mOverlayService = IOverlayManager.Stub
+                .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
+        setupAccentPref();
+        setupGradientPref();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
 
@@ -123,4 +142,62 @@ public class UserInterface extends SettingsPreferenceFragment {
                     return keys;
                 }
             };
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mThemeColor) {
+            int color = (Integer) objValue;
+            String hexColor = String.format("%08X", (0xFFFFFFFF & color));
+            SystemProperties.set(ACCENT_COLOR_PROP, hexColor);
+            try {
+                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
+             } catch (RemoteException ignored) {
+             }
+        } else if (preference == mGradientColor) {
+            int color = (Integer) objValue;
+            String hexColor = String.format("%08X", (0xFFFFFFFF & color));
+            SystemProperties.set(GRADIENT_COLOR_PROP, hexColor);
+            try {
+                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
+             } catch (RemoteException ignored) {
+             }
+        }
+        return true;
+        }
+
+    private void setupAccentPref() {
+        mThemeColor = (ColorPickerPreference) findPreference(ACCENT_COLOR);
+        String colorVal = SystemProperties.get(ACCENT_COLOR_PROP, "-1");
+        int color = "-1".equals(colorVal)
+                ? DEFAULT_COLOR
+                : Color.parseColor("#" + colorVal);
+        mThemeColor.setNewPreviewColor(color);
+        mThemeColor.setOnPreferenceChangeListener(this);
+        String hexColor = String.format("%08X", (0xFFFFFFFF & color));
+        if (hexColor.equals("0xff1a73e8")) {
+            mThemeColor.setSummary(R.string.default_string);
+        } else {
+            mThemeColor.setSummary(hexColor);
+        }
+    }
+
+    private void setupGradientPref() {
+        mGradientColor = (ColorPickerPreference) findPreference(GRADIENT_COLOR);
+        String colorVal = SystemProperties.get(GRADIENT_COLOR_PROP, "-1");
+        int color = "-1".equals(colorVal)
+                ? DEFAULT_COLOR
+                : Color.parseColor("#" + colorVal);
+        mGradientColor.setNewPreviewColor(color);
+        mGradientColor.setOnPreferenceChangeListener(this);
+        String hexColor = String.format("%08X", (0xFFFFFFFF & color));
+        if (hexColor.equals("0xff1a73e8")) {
+            mGradientColor.setSummary(R.string.default_string);
+        } else {
+            mGradientColor.setSummary(hexColor);
+        }
+    }
 }
