dec9f367ab86f63800744c9d8736faf93dbacbc3
Pranav Vashi <neobuddy89@gmail.com>
2021-12-04 18:13:14 +0530

    crdroid: Set summary automatically for color picker pref
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/preferences/colorpicker/ColorPickerPreference.java
+++ src/com/crdroid/settings/preferences/colorpicker/ColorPickerPreference.java
@@ -2,6 +2,7 @@
  * Copyright (C) 2011 Sergey Margaritov
  * Copyright (C) 2013 Slimroms
  * Copyright (C) 2015 The TeamEos Project
+ * Copyright (C) 2020-2021 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +33,8 @@ import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
-import androidx.preference.*;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -61,12 +63,14 @@ public class ColorPickerPreference extends Preference implements
     ColorPickerDialog mDialog;
     private int mDefaultValue = Color.BLACK;
     private int mCurrentValue = mDefaultValue;
+    private String mCurrentHexValue;
     private float mDensity = 0;
     private boolean mAlphaSliderEnabled = false;
     private boolean mShowReset;
     private boolean mShowPreview;
     private boolean mDividerAbove;
     private boolean mDividerBelow;
+    private boolean mAutoSummary = true;
     private EditText mEditText;
 
     public ColorPickerPreference(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -104,6 +108,8 @@ public class ColorPickerPreference extends Preference implements
             defaultValue = Color.BLACK;
         }
         mCurrentValue = getPersistedInt((Integer) defaultValue);
+        mCurrentHexValue = convertToARGB((Integer) defaultValue);
+        if (mAutoSummary) setSummary(mCurrentHexValue);
         onColorChanged(mCurrentValue);
     }
 
@@ -215,6 +221,8 @@ public class ColorPickerPreference extends Preference implements
     @Override
     public void onColorChanged(int color) {
         mCurrentValue = color;
+        mCurrentHexValue = convertToARGB(color);
+        if (mAutoSummary) setSummary(mCurrentHexValue);
         setPreviewColor();
         persistInt(color);
         try {
@@ -328,6 +336,15 @@ public class ColorPickerPreference extends Preference implements
         }
 
         return Color.argb(alpha, red, green, blue);
+    }
+
+    /**
+     * Toggle Auto Summary (by default it's enabled)
+     *
+     * @param enable
+     */
+    public void setAutoSummaryEnabled(boolean enable) {
+        mAutoSummary = enable;
     }
 
     @Override
