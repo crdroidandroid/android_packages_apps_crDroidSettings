dc9a09855626b21a7e8a8c0a6270e789e113b09e
Pranav Vashi <neobuddy89@gmail.com>
2021-12-14 14:50:49 +0530

    Battery Styles: Add customization for QS
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -69,6 +69,14 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.SHOW_QS_CLOCK, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.SHOW_QS_DATE, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_BATTERY_STYLE, -1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_BATTERY_LOCATION, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_SHOW_BATTERY_PERCENT, 2, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_SHOW_BATTERY_ESTIMATE, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
