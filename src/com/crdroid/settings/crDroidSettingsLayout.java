c4b05cbc9b0c7640f70d7d5f45bc185f4596aa7f
Pranav Vashi <neobuddy89@gmail.com>
2021-12-27 20:45:06 +0530

    crdroid: Add navigation fragment
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/crDroidSettingsLayout.java
+++ src/com/crdroid/settings/crDroidSettingsLayout.java
@@ -44,6 +44,7 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.crdroid.settings.fragments.About;
 import com.crdroid.settings.fragments.Buttons;
 import com.crdroid.settings.fragments.LockScreen;
+import com.crdroid.settings.fragments.Navigation;
 import com.crdroid.settings.fragments.QuickSettings;
 import com.crdroid.settings.fragments.Sound;
 import com.crdroid.settings.fragments.StatusBar;
@@ -51,7 +52,6 @@ import com.crdroid.settings.fragments.UserInterface;
 
 /*
 import com.crdroid.settings.fragments.Miscellaneous;
-import com.crdroid.settings.fragments.Navigation;
 import com.crdroid.settings.fragments.Notifications;
 */
 
@@ -136,11 +136,11 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
         protected Void doInBackground(Void... params) {
 /*
             Miscellaneous.reset(rContext);
-            Navigation.reset(rContext);
             Notifications.reset(rContext);
 */
             Buttons.reset(rContext);
             LockScreen.reset(rContext);
+            Navigation.reset(rContext);
             QuickSettings.reset(rContext);
             Sound.reset(rContext);
             StatusBar.reset(rContext);
@@ -170,18 +170,17 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
 /*
-            frags[2] = new LockScreen();
-            frags[3] = new Navigation();
             frags[6] = new Notifications();
             frags[8] = new Miscellaneous();
 */
             frags[0] = new StatusBar();
             frags[1] = new QuickSettings();
             frags[2] = new LockScreen();
-            frags[3] = new Buttons();
-            frags[4] = new UserInterface();
-            frags[5] = new Sound();
-            frags[6] = new About();
+            frags[3] = new Navigation();
+            frags[4] = new Buttons();
+            frags[5] = new UserInterface();
+            frags[6] = new Sound();
+            frags[7] = new About();
         }
 
         @Override
@@ -204,13 +203,13 @@ public class crDroidSettingsLayout extends SettingsPreferenceFragment {
         String titleString[];
         titleString = new String[] {
 /*
-            getString(R.string.navbar_title),
             getString(R.string.notifications_title),
             getString(R.string.misc_title),
 */
             getString(R.string.statusbar_title),
             getString(R.string.quicksettings_title),
             getString(R.string.lockscreen_title),
+            getString(R.string.navigation_title),
             getString(R.string.button_title),
             getString(R.string.ui_title),
             getString(R.string.sound_title),
---
+++ src/com/crdroid/settings/fragments/Navigation.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2016-2021 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.crdroid.settings.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.crdroid.Utils;
+
+import lineageos.providers.LineageSettings;
+
+@SearchIndexable
+public class Navigation extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    public static final String TAG = "Navigation";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.crdroid_settings_navigation);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CRDROID_SETTINGS;
+    }
+
+    /**
+     * For search
+     */
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.crdroid_settings_navigation);
+}
