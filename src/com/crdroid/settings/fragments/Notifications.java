c3c139cf2a9a65f66e27f5a7747a30b7f9f96316
Pranav Vashi <neobuddy89@gmail.com>
2022-01-16 13:15:21 +0400

    Allow to suppress notifications sound/vibration if screen is ON [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- src/com/crdroid/settings/fragments/Notifications.java
+++ src/com/crdroid/settings/fragments/Notifications.java
@@ -86,6 +86,8 @@ public class Notifications extends SettingsPreferenceFragment {
                 Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, 1);
         Settings.System.putIntForUser(resolver,
                 Settings.System.LESS_BORING_HEADS_UP, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON, 1, UserHandle.USER_CURRENT);
     }
 
     @Override
