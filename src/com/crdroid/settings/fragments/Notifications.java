5488f9cb7b0caf8c84a83e12ee8d152424cac3f9
Pranav Vashi <neobuddy89@gmail.com>
2022-01-21 10:29:40 +0530

    crdroid: Add Flashlight blink on incoming calls [2/3]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/Notifications.java
+++ src/com/crdroid/settings/fragments/Notifications.java
@@ -51,6 +51,7 @@ public class Notifications extends SettingsPreferenceFragment {
     private static final String ALERT_SLIDER_PREF = "alert_slider_notifications";
     private static final String BATTERY_LIGHTS_PREF = "battery_lights";
     private static final String NOTIFICATION_LIGHTS_PREF = "notification_lights";
+    private static final String FLASHLIGHT_CALL_PREF = "flashlight_on_call";
 
     private Preference mAlertSlider;
     private Preference mBatLights;
@@ -90,6 +91,12 @@ public class Notifications extends SettingsPreferenceFragment {
             lightsCategory = (PreferenceCategory) prefScreen.findPreference("light_brightness");
             prefScreen.removePreference(lightsCategory);
         }
+
+        if (!Utils.deviceHasFlashlight(mContext)) {
+            final Preference flashlightcall =
+                    (Preference) prefScreen.findPreference(FLASHLIGHT_CALL_PREF);
+            prefScreen.removePreference(flashlightcall);
+        }
     }
 
     public static void reset(Context mContext) {
@@ -106,6 +113,8 @@ public class Notifications extends SettingsPreferenceFragment {
                 Settings.System.NOTIFICATION_SOUND_VIB_SCREEN_ON, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.POWER_MENU_BG_ALPHA, 255, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.FLASHLIGHT_ON_CALL, 0, UserHandle.USER_CURRENT);
         Ticker.reset(mContext);
     }
 
@@ -139,6 +148,10 @@ public class Notifications extends SettingsPreferenceFragment {
                             com.android.internal.R.bool.config_intrusiveNotificationLed);
                     if (!mNotLightsSupported)
                         keys.add(NOTIFICATION_LIGHTS_PREF);
+
+                    if (!Utils.deviceHasFlashlight(context)) {
+                        keys.add(FLASHLIGHT_CALL_PREF);
+                    }
 
                     return keys;
                 }
