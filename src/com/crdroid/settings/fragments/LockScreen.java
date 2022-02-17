b980c2105d4a65124b2a9eaffd130e983126b0f7
Pranav Vashi <neobuddy89@gmail.com>
2022-02-17 22:31:59 +0530

    omni wip
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/LockScreen.java
+++ src/com/crdroid/settings/fragments/LockScreen.java
@@ -102,6 +102,8 @@ public class LockScreen extends SettingsPreferenceFragment
                 Settings.System.FP_ERROR_VIBRATE, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.FP_SUCCESS_VIBRATE, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_WEATHER_ENABLED, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
