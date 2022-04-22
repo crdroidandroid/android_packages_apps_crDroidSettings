1e267bc06930c29955e688d6b9afdaa8e69bbeec
Pranav Vashi <neobuddy89@gmail.com>
2022-04-22 19:58:16 +0530

    Add strings for hiding back arrow animation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/Navigation.java
+++ src/com/crdroid/settings/fragments/Navigation.java
@@ -215,6 +215,8 @@ public class Navigation extends SettingsPreferenceFragment implements
                 Settings.Secure.NAVBAR_INVERSE_LAYOUT, 0, UserHandle.USER_CURRENT);
         Settings.Secure.putStringForUser(resolver,
                 Settings.Secure.NAVBAR_LAYOUT_VIEWS, "default", UserHandle.USER_CURRENT);
+        Settings.Secure.putIntForUser(resolver,
+                Settings.Secure.BACK_GESTURE_ARROW, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.PIXEL_NAV_ANIMATION, 1, UserHandle.USER_CURRENT);
     }
