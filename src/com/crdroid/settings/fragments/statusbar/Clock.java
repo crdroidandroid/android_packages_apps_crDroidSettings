dca101eef96c8333826fea3136107d5143dd0a76
Pranav Vashi <neobuddy89@gmail.com>
2022-08-29 23:25:03 +0530

    Statusbar Clock background chip [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/statusbar/Clock.java
+++ src/com/crdroid/settings/fragments/statusbar/Clock.java
@@ -225,6 +225,8 @@ public class Clock extends SettingsPreferenceFragment
                 Settings.System.STATUS_BAR_CLOCK_DATE_FORMAT, "");
         Settings.System.putIntForUser(resolver,
                 Settings.System.STATUS_BAR_CLOCK_SECONDS, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.STATUSBAR_CLOCK_CHIP, 0, UserHandle.USER_CURRENT);
         LineageSettings.System.putIntForUser(resolver,
                 LineageSettings.System.STATUS_BAR_AM_PM, 0, UserHandle.USER_CURRENT);
         LineageSettings.System.putIntForUser(resolver,
