c356bedb0ade37944932d7d2a077921c383284c8
Pranav Vashi <neobuddy89@gmail.com>
2022-10-15 12:42:14 +0530

    crdroid: Make weather location on lockscreen optional [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/LockScreen.java
+++ src/com/crdroid/settings/fragments/LockScreen.java
@@ -128,6 +128,8 @@ public class LockScreen extends SettingsPreferenceFragment
                 Settings.System.LOCKSCREEN_ENABLE_POWER_MENU, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.LOCKSCREEN_WEATHER_ENABLED, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCKSCREEN_WEATHER_LOCATION, 0, UserHandle.USER_CURRENT);
         UdfpsSettings.reset(mContext);
     }
 
