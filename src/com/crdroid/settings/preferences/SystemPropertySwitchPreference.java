31011e5695f83436dee839d72ad2a9859d4534f3
Pranav Vashi <neobuddy89@gmail.com>
2022-11-21 02:16:42 +0530

    crdroid: Add helper preference for system properties
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

---
+++ src/com/crdroid/settings/preferences/SystemPropertySwitchPreference.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2022 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.crdroid.settings.preferences;
+
+import android.content.Context;
+import android.os.SystemProperties;
+import android.util.AttributeSet;
+
+import com.android.settingslib.development.SystemPropPoker;
+
+import lineageos.preference.SelfRemovingSwitchPreference;
+
+public class SystemPropertySwitchPreference extends SelfRemovingSwitchPreference {
+
+    public SystemPropertySwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SystemPropertySwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SystemPropertySwitchPreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected boolean isPersisted() {
+        return SystemProperties.get(getKey(), null) != null;
+    }
+
+    @Override
+    protected void putBoolean(String key, boolean value) {
+        SystemProperties.set(key, Boolean.toString(value));
+        SystemPropPoker.getInstance().poke();
+    }
+
+    @Override
+    protected boolean getBoolean(String key, boolean defaultValue) {
+        return SystemProperties.getBoolean(key, defaultValue);
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        boolean defValue = defaultValue == null ? false : (Boolean) defaultValue;
+        setChecked(restoreValue ? getBoolean(getKey(), defValue) : defValue);
+    }
+}
