a143fb8a0f4c22023270fc517967c3987d5748fb
Ido Ben-Hur <idoybh2@gmail.com>
2022-12-03 15:28:40 +0200

    crdroid: Add native monet settings to change theme style
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/ui/MonetSettings.java
+++ src/com/crdroid/settings/fragments/ui/MonetSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2023 crDroid Android Project
+ * Copyright (C) 2021-2024 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,26 +20,30 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.os.UserManager;
 import android.provider.Settings;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.SwitchPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import lineageos.providers.LineageSettings;
 
-public class MonetSettings extends SettingsPreferenceFragment {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MonetSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
 
     final static String TAG = "MonetSettings";
 
+    private static final String OVERLAY_CATEGORY_THEME_STYLE =
+            "android.theme.customization.theme_style";
+
+    private static final String PREF_THEME_STYLE = "theme_style";
     private static final String PREF_CHROMA_FACTOR ="monet_engine_chroma_factor";
     private static final String PREF_LUMINANCE_FACTOR ="monet_engine_luminance_factor";
     private static final String PREF_TINT_BACKGROUND ="monet_engine_tint_background";
@@ -48,12 +52,95 @@ public class MonetSettings extends SettingsPreferenceFragment {
     private static final String PREF_CUSTOM_BGCOLOR ="monet_engine_custom_bgcolor";
     private static final String PREF_BGCOLOR_OVERRIDE ="monet_engine_bgcolor_override";
 
+    private ListPreference mThemeStylePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.monet_engine);
+
+        mThemeStylePref = findPreference(PREF_THEME_STYLE);
+        updatePreferences();
+        mThemeStylePref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mThemeStylePref) {
+            String value = (String) newValue;
+            setStyleValue(value);
+            updateListByValue(mThemeStylePref, value, false);
+            return true;
+        }
+        return false;
+    }
+
+    private void updatePreferences() {
+        final String overlayPackageJson = Settings.Secure.getStringForUser(
+                getActivity().getContentResolver(),
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+                UserHandle.USER_CURRENT);
+        if (overlayPackageJson != null && !overlayPackageJson.isEmpty()) {
+            try {
+                final JSONObject object = new JSONObject(overlayPackageJson);
+                final String style = object.optString(OVERLAY_CATEGORY_THEME_STYLE, null);
+                // style handling
+                boolean styleUpdated = false;
+                if (style != null && !style.isEmpty()) {
+                    for (CharSequence value : mThemeStylePref.getEntryValues()) {
+                        if (value.toString().equals(style)) {
+                            styleUpdated = true;
+                            break;
+                        }
+                    }
+                    if (styleUpdated) {
+                        updateListByValue(mThemeStylePref, style);
+                    }
+                }
+                if (!styleUpdated) {
+                    updateListByValue(mThemeStylePref,
+                            mThemeStylePref.getEntryValues()[0].toString());
+                }
+            } catch (JSONException | IllegalArgumentException ignored) {}
+        }
+    }
+
+    private void updateListByValue(ListPreference pref, String value) {
+        updateListByValue(pref, value, true);
+    }
+
+    private void updateListByValue(ListPreference pref, String value, boolean set) {
+        if (set) pref.setValue(value);
+        final int index = pref.findIndexOfValue(value);
+        pref.setSummary(pref.getEntries()[index]);
+    }
+
+    private JSONObject getSettingsJson() throws JSONException {
+        final String overlayPackageJson = Settings.Secure.getStringForUser(
+                getActivity().getContentResolver(),
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+                UserHandle.USER_CURRENT);
+        JSONObject object;
+        if (overlayPackageJson == null || overlayPackageJson.isEmpty())
+            return new JSONObject();
+        return new JSONObject(overlayPackageJson);
+    }
+
+    private void putSettingsJson(JSONObject object) {
+        Settings.Secure.putStringForUser(
+                getActivity().getContentResolver(),
+                Settings.Secure.THEME_CUSTOMIZATION_OVERLAY_PACKAGES,
+                object.toString(), UserHandle.USER_CURRENT);
+    }
+
+    private void setStyleValue(String style) {
+        try {
+            JSONObject object = getSettingsJson();
+            object.putOpt(OVERLAY_CATEGORY_THEME_STYLE, style);
+            putSettingsJson(object);
+        } catch (JSONException | IllegalArgumentException ignored) {}
     }
 
     public static void reset(Context mContext) {
