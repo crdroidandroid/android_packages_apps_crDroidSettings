b30cbcf6c53f41f604d64468bb271c9379799f24
ShevT <evgeny.shishkov@gmail.com>
2022-12-06 07:36:18 +0300

    Revert "crdroid: Power menu styles [2/2]"
     Reason: NPE when using A11 style (at minimum). Colors problem.
     P.S. Maybe I'll come back to this later.
    
    This reverts commit ceef5d29e110688bd61f1c9e9f594b5b0a599ccb.

--- src/com/crdroid/settings/fragments/buttons/PowerMenuActions.java
+++ src/com/crdroid/settings/fragments/buttons/PowerMenuActions.java
@@ -226,8 +226,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 LineageSettings.Secure.ADVANCED_REBOOT, 1, UserHandle.USER_CURRENT);
         LineageSettings.Secure.putIntForUser(resolver,
                 LineageSettings.Secure.ADVANCED_REBOOT_SECURED, 1, UserHandle.USER_CURRENT);
-        Settings.Secure.putIntForUser(resolver,
-                Settings.Secure.POWER_MENU_TYPE, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
