36e88c9d17102390b079a14b0145a4283fce464c
Pranav Vashi <neobuddy89@gmail.com>
2023-12-22 01:48:58 +0530

    crdroid: Bring back QS Header images
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -200,6 +200,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.QS_DUAL_TONE, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_SHOW_DATA_USAGE, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_HEADER_IMAGE, 0, UserHandle.USER_CURRENT);
         LineageSettings.Secure.putIntForUser(resolver,
                 LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT);
         LineageSettings.Secure.putIntForUser(resolver,
