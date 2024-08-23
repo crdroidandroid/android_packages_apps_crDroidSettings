7824a3a206e0d96b99b738f076d7c5f71c3c16a4
Pranav Vashi <neobuddy89@gmail.com>
2024-08-24 03:09:24 +0530

    crdroid: DeviceUtils: Clean up unused functions
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/utils/DeviceUtils.java
+++ src/com/crdroid/settings/utils/DeviceUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2022 crDroid Android Project
+ * Copyright (C) 2016-2024 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,27 +15,13 @@
  */
 package com.crdroid.settings.utils;
 
-import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_2BUTTON;
-import static android.view.WindowManagerPolicyConstants.NAV_BAR_MODE_GESTURAL;
-
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.bluetooth.BluetoothAdapter;
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Point;
 import android.graphics.Rect;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
-import android.hardware.fingerprint.FingerprintManager;
-import android.hardware.fingerprint.FingerprintSensorPropertiesInternal;
-import android.net.ConnectivityManager;
-import android.nfc.NfcAdapter;
 import android.os.Build;
 import android.os.SystemProperties;
 import android.provider.Settings;
@@ -45,8 +31,6 @@ import android.view.DisplayCutout;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
 import android.view.Surface;
-
-import java.util.List;
 
 import static org.lineageos.internal.util.DeviceKeysConstants.*;
 
@@ -185,78 +169,6 @@ public class DeviceUtils {
                 .config_deviceSupportsKeyboardBrightnessControl) != 0;
     }
 
-    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
-        if (pkg != null) {
-            try {
-                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
-                if (!pi.applicationInfo.enabled && !ignoreState) {
-                    return false;
-                }
-            } catch (PackageManager.NameNotFoundException e) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Locks the activity orientation to the current device orientation
-     * @param activity
-     */
-    public static void lockCurrentOrientation(Activity activity) {
-        int currentRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
-        int orientation = activity.getResources().getConfiguration().orientation;
-        int frozenRotation = 0;
-        switch (currentRotation) {
-            case Surface.ROTATION_0:
-                frozenRotation = orientation == Configuration.ORIENTATION_LANDSCAPE
-                        ? ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-                        : ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
-                break;
-            case Surface.ROTATION_90:
-                frozenRotation = orientation == Configuration.ORIENTATION_PORTRAIT
-                        ? ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT
-                        : ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
-                break;
-            case Surface.ROTATION_180:
-                frozenRotation = orientation == Configuration.ORIENTATION_LANDSCAPE
-                        ? ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE
-                        : ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
-                break;
-            case Surface.ROTATION_270:
-                frozenRotation = orientation == Configuration.ORIENTATION_PORTRAIT
-                        ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-                        : ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
-                break;
-        }
-        activity.setRequestedOrientation(frozenRotation);
-    }
-
-    public static boolean isDozeAvailable(Context context) {
-        String name = Build.IS_DEBUGGABLE ? SystemProperties.get("debug.doze.component") : null;
-        if (TextUtils.isEmpty(name)) {
-            name = context.getResources().getString(
-                    com.android.internal.R.string.config_dozeComponent);
-        }
-        return !TextUtils.isEmpty(name);
-    }
-
-/*
-    public static boolean deviceSupportsMobileData(Context ctx) {
-        ConnectivityManager cm = ctx.getSystemService(ConnectivityManager.class);
-        return cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
-    }
-*/
-
-    public static boolean deviceSupportsBluetooth() {
-        return (BluetoothAdapter.getDefaultAdapter() != null);
-    }
-
-    public static boolean deviceSupportsNfc(Context ctx) {
-        return NfcAdapter.getDefaultAdapter(ctx) != null;
-    }
-
     public static boolean deviceSupportsFlashLight(Context context) {
         CameraManager cameraManager = context.getSystemService(CameraManager.class);
         try {
@@ -272,43 +184,9 @@ public class DeviceUtils {
                     return true;
                 }
             }
-        } catch (ArrayIndexOutOfBoundsException | CameraAccessException | AssertionError e) {
+        } catch (Exception | AssertionError e) {
             // Ignore
         }
         return false;
-    }
-
-    public static boolean isSwipeUpEnabled(Context context) {
-        if (isEdgeToEdgeEnabled(context)) {
-            return false;
-        }
-        return NAV_BAR_MODE_2BUTTON == context.getResources().getInteger(
-                com.android.internal.R.integer.config_navBarInteractionMode);
-    }
-
-    public static boolean isEdgeToEdgeEnabled(Context context) {
-        return NAV_BAR_MODE_GESTURAL == context.getResources().getInteger(
-                com.android.internal.R.integer.config_navBarInteractionMode);
-    }
-
-    public static boolean isBlurSupported() {
-        boolean blurSupportedSysProp = SystemProperties
-            .getBoolean("ro.surface_flinger.supports_background_blur", false);
-        boolean blurDisabledSysProp = SystemProperties
-            .getBoolean("persist.sys.sf.disable_blurs", false);
-        return blurSupportedSysProp && !blurDisabledSysProp && ActivityManager.isHighEndGfx();
-    }
-
-     /**
-     * Checks if the device has udfps
-     * @param context context for getting FingerprintManager
-     * @return true is udfps is present
-     */
-    public static boolean hasUDFPS(Context context) {
-        final FingerprintManager fingerprintManager =
-                context.getSystemService(FingerprintManager.class);
-        final List<FingerprintSensorPropertiesInternal> props =
-                fingerprintManager.getSensorPropertiesInternal();
-        return props != null && props.size() == 1 && props.get(0).isAnyUdfpsType();
     }
 }
