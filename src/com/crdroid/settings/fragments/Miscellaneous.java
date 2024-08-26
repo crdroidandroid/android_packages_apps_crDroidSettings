1df960e5b1c783706010ed70d491f1c692d88b1e
Pranav Vashi <neobuddy89@gmail.com>
2024-08-26 12:08:20 +0530

    crdroid: Remove default action for three finger swipe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/Miscellaneous.java
+++ src/com/crdroid/settings/fragments/Miscellaneous.java
@@ -71,11 +71,9 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
         if (!mPocketJudgeSupported)
             prefScreen.removePreference(mPocketJudge);
 
-        Action defaultThreeFingersSwipeAction = Action.fromIntSafe(res.getInteger(
-                org.lineageos.platform.internal.R.integer.config_threeFingersSwipeBehavior));
         Action threeFingersSwipeAction = Action.fromSettings(getContentResolver(),
                 LineageSettings.System.KEY_THREE_FINGERS_SWIPE_ACTION,
-                defaultThreeFingersSwipeAction);
+                Action.NOTHING);
         mThreeFingersSwipeAction = initList(KEY_THREE_FINGERS_SWIPE, threeFingersSwipeAction);
     }
 
