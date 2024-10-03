14d2e23bf2729c2b89f80f1f64941ca21add53e0
Pranav Vashi <neobuddy89@gmail.com>
2024-10-04 01:04:45 +0530

    crdroid: Update network traffic settings to use main switch
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/statusbar/NetworkTrafficSettings.java
+++ src/com/crdroid/settings/fragments/statusbar/NetworkTrafficSettings.java
@@ -21,78 +21,20 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.UserHandle;
 
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.SwitchPreferenceCompat;
-
 import lineageos.providers.LineageSettings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
 
-import com.crdroid.settings.preferences.CustomSeekBarPreference;
-
-public class NetworkTrafficSettings extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener  {
+public class NetworkTrafficSettings extends SettingsPreferenceFragment {
 
     private static final String TAG = "NetworkTrafficSettings";
-
-    private CustomSeekBarPreference mNetTrafficAutohideThreshold;
-    private CustomSeekBarPreference mNetTrafficRefreshInterval;
-    private ListPreference mNetTrafficLocation;
-    private ListPreference mNetTrafficMode;
-    private ListPreference mNetTrafficUnits;
-    private SwitchPreferenceCompat mNetTrafficAutohide;
-    private SwitchPreferenceCompat mNetTrafficHideArrow;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.network_traffic_settings);
-        final ContentResolver resolver = getActivity().getContentResolver();
-
-        mNetTrafficAutohideThreshold = (CustomSeekBarPreference)
-                findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
-        mNetTrafficRefreshInterval = (CustomSeekBarPreference)
-                findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_REFRESH_INTERVAL);
-        mNetTrafficLocation = (ListPreference)
-                findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_LOCATION);
-        mNetTrafficLocation.setOnPreferenceChangeListener(this);
-        mNetTrafficMode = (ListPreference)
-                findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_MODE);
-        mNetTrafficAutohide = (SwitchPreferenceCompat)
-                findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE);
-        mNetTrafficUnits = (ListPreference)
-                findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_UNITS);
-        mNetTrafficHideArrow = (SwitchPreferenceCompat)
-                findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_HIDEARROW);
-
-        int location = LineageSettings.Secure.getIntForUser(resolver,
-                LineageSettings.Secure.NETWORK_TRAFFIC_LOCATION, 0, UserHandle.USER_CURRENT);
-        updateEnabledStates(location);
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mNetTrafficLocation) {
-            int location = Integer.valueOf((String) newValue);
-            updateEnabledStates(location);
-            return true;
-        }
-        return false;
-    }
-
-    private void updateEnabledStates(int location) {
-        final boolean enabled = location != 0;
-        mNetTrafficMode.setEnabled(enabled);
-        mNetTrafficAutohide.setEnabled(enabled);
-        mNetTrafficAutohideThreshold.setEnabled(enabled);
-        mNetTrafficHideArrow.setEnabled(enabled);
-        mNetTrafficRefreshInterval.setEnabled(enabled);
-        mNetTrafficUnits.setEnabled(enabled);
     }
 
     public static void reset(Context mContext) {
