a5d388e78c8dc3e5a2acb7fadbf4b669c9986e72
kamikaonashi <thorschi93@gmx.at>
2025-12-04 12:05:56 +0100

    crdroid: Extend QS Header Image to support animated GIF/WEBP [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/quicksettings/QsHeaderImageSettings.java
+++ src/com/crdroid/settings/fragments/quicksettings/QsHeaderImageSettings.java
@@ -150,6 +150,8 @@ public class QsHeaderImageSettings extends SettingsPreferenceFragment implements
             try {
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_MIME_TYPES,
+                        new String[]{"image/jpeg","image/png","image/gif","image/webp"});
                 startActivityForResult(intent, REQUEST_PICK_IMAGE);
                 return true;
             } catch (Exception e) {
