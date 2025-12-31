5a83cb4927df1a52093867d4359aacd71494ac50
Pranav Vashi <neobuddy89@gmail.com>
2025-12-31 08:19:51 +0530

    crdroid: Initial battery styles
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/StatusBar.java
+++ src/com/crdroid/settings/fragments/StatusBar.java
@@ -127,6 +127,8 @@ public class StatusBar extends SettingsPreferenceFragment implements
         LineageSettings.System.putIntForUser(resolver,
                 LineageSettings.System.STATUS_BAR_CLOCK, 0, UserHandle.USER_CURRENT);
         LineageSettings.System.putIntForUser(resolver,
+                LineageSettings.System.STATUS_BAR_BATTERY_STYLE, 0, UserHandle.USER_CURRENT);
+        LineageSettings.System.putIntForUser(resolver,
                 LineageSettings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
         Settings.Secure.putIntForUser(resolver,
                 Settings.Secure.ENABLE_CAMERA_PRIVACY_INDICATOR, 1, UserHandle.USER_CURRENT);
