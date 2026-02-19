b65742c9083ced31acc57cca6efebc5f003238e1
Pranav Vashi <neobuddy89@gmail.com>
2026-02-19 17:58:52 +0530

    Revert "crdroid: Disable QS footer settings"
    This reverts commit f7f0f249cb6a7101393fe43af5afebfb9e70d456
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -132,6 +132,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_BRIGHTNESS_SLIDER_SHAPE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
+                Settings.System.QS_FOOTER_SHOW_SETTINGS, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_FOOTER_SHOW_EDIT, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_FOOTER_SHOW_POWER_MENU, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
                 Settings.System.QS_SHOW_DATA_USAGE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_TILE_HAPTIC, 1, UserHandle.USER_CURRENT);
