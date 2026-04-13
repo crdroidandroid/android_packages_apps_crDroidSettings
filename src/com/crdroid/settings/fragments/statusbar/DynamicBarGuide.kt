79adafc9aa4a100e7ea061fee3445ecb3d6e33dc
Pranav Vashi <neobuddy89@gmail.com>
2026-04-13 16:48:48 +0530

    crdroid: Improve dynamic bar guide steps layout
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/crdroid/settings/fragments/statusbar/DynamicBarGuide.kt
+++ src/com/crdroid/settings/fragments/statusbar/DynamicBarGuide.kt
@@ -6,6 +6,7 @@
 package com.crdroid.settings.fragments.statusbar
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import com.android.internal.logging.nano.MetricsProto
 import com.android.settings.R
@@ -14,14 +15,14 @@ import com.android.settingslib.widget.LayoutPreference
 
 class DynamicBarGuide : SettingsPreferenceFragment() {
 
-    private data class GuideStep(val titleRes: Int, val bodyRes: Int)
+    private data class GuideStep(val titleRes: Int, val bodyRes: Int, val iconRes: Int)
 
     private val guideSteps = listOf(
-        GuideStep(R.string.dynamic_bar_guide_chip_title, R.string.dynamic_bar_guide_chip),
-        GuideStep(R.string.dynamic_bar_guide_swipe_title, R.string.dynamic_bar_guide_swipe),
-        GuideStep(R.string.dynamic_bar_guide_expand_title, R.string.dynamic_bar_guide_expand),
-        GuideStep(R.string.dynamic_bar_guide_dismiss_title, R.string.dynamic_bar_guide_dismiss),
-        GuideStep(R.string.dynamic_bar_guide_keyguard_title, R.string.dynamic_bar_guide_keyguard),
+        GuideStep(R.string.dynamic_bar_guide_chip_title, R.string.dynamic_bar_guide_chip, R.drawable.ic_fiber_manual_record),
+        GuideStep(R.string.dynamic_bar_guide_swipe_title, R.string.dynamic_bar_guide_swipe, R.drawable.ic_swap_horiz),
+        GuideStep(R.string.dynamic_bar_guide_expand_title, R.string.dynamic_bar_guide_expand, R.drawable.ic_touch_app),
+        GuideStep(R.string.dynamic_bar_guide_dismiss_title, R.string.dynamic_bar_guide_dismiss, R.drawable.ic_expand_less),
+        GuideStep(R.string.dynamic_bar_guide_keyguard_title, R.string.dynamic_bar_guide_keyguard, R.drawable.ic_notifications),
     )
 
     private val stepViewIds = intArrayOf(
@@ -49,6 +50,7 @@ class DynamicBarGuide : SettingsPreferenceFragment() {
         guideSteps.forEachIndexed { index, step ->
             val stepView = layoutPref.findViewById<android.view.View>(stepViewIds[index])
                 ?: return@forEachIndexed
+            stepView.findViewById<ImageView>(R.id.step_icon)?.setImageResource(step.iconRes)
             stepView.findViewById<TextView>(R.id.step_number)?.text = "${index + 1}"
             stepView.findViewById<TextView>(R.id.step_title)?.setText(step.titleRes)
             stepView.findViewById<TextView>(R.id.step_body)?.setText(step.bodyRes)
